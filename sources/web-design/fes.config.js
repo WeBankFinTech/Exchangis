2a6ae66f450ca2bff9aacd87ef75f186636ddc11
zwx-master <1208247854@qq.com>
2020-10-23 18:50:03 +0800

    web source code

---
+++ sources/web-design/fes.config.js
@@ -0,0 +1,90 @@
+import zh from './zh.json';
+import en from './en.json';
+
+const lang = window.localStorage.getItem('lang') || 'zh-cn';
+export default {
+    mode: 'vertical',
+    theme: 'dark',
+    fesName: 'Exchangis',
+    roles: {
+        unLogin: ['/login', '/ds/newManager', '/job', '/task', '/help'],
+        user: ['/login', '/ds/newManager', '/ds/dataTemplate', '/job', '/task', '/help', '/group', '/group/groupDetail'],
+        admin: ['/login', '/ds/newManager', '/ds/dataTemplate', '/job', '/task', '/help', '/group', '/group/groupDetail', '/sys/owner'],
+        super: ['/login', '/ds/newManager', '*']
+    },
+    map: {
+        status: [
+            ['1', '成功'],
+            ['2', '失败']
+        ]
+    },
+    i18n: {
+        locale: lang,
+        messages: {
+            en,
+            'zh-cn': zh
+        }
+    },
+    menu: [{
+        title: '$i18n.navBar.dataManager',
+        path: '/ds',
+        subMenu: [{
+            title: '$i18n.navBar.dataSource',
+            path: '/ds/newManager'
+        },
+        {
+            title: '$i18n.navBar.templaceManagert',
+            path: '/ds/dataTemplate'
+        }
+        ]
+    },
+    {
+        title: '$i18n.navBar.taskConfig',
+        path: '/job'
+    },
+    {
+        title: '$i18n.navBar.tasking',
+        path: '/task'
+    },
+    {
+        title: '$i18n.navBar.group',
+        path: '/group'
+    },
+    // {
+    //     title: '$i18n.navBar.help',
+    //     path: '/help'
+    // },
+    {
+        title: '$i18n.navBar.sysManagert',
+        path: '/sys',
+        subMenu: [{
+            title: '$i18n.navBar.owner',
+            path: '/sys/owner'
+        },
+        {
+            title: '$i18n.navBar.execotor',
+            path: '/sys/executor'
+        },
+        {
+            title: '$i18n.navBar.executeUser',
+            path: '/sys/executeUser'
+        },
+        {
+            title: '$i18n.navBar.user',
+            path: '/sys/user'
+        }
+        ]
+    }
+    ],
+    env: {
+        local: {
+            api: '' // 本地联调地址
+        },
+        prod: {
+            api: '/api/v1' // 生产环境
+        },
+        open: {
+            api: '/api/v1'
+        }
+    }
+};
