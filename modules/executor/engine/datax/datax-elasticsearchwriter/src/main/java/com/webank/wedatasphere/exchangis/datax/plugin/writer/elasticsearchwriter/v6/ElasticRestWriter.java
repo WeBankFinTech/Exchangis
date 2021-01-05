3ab2c8afd72b40195b9b1ad3a03898426ffae872
77954309 <77954309@qq.com>
2021-01-05 14:59:14 +0800

    ElasticRestHighClient 添加 创建索引，字段

--- modules/executor/engine/datax/datax-elasticsearchwriter/src/main/java/com/webank/wedatasphere/exchangis/datax/plugin/writer/elasticsearchwriter/v6/ElasticRestWriter.java
+++ modules/executor/engine/datax/datax-elasticsearchwriter/src/main/java/com/webank/wedatasphere/exchangis/datax/plugin/writer/elasticsearchwriter/v6/ElasticRestWriter.java
@@ -4,18 +4,29 @@ import com.alibaba.datax.common.element.Record;
 import com.alibaba.datax.common.exception.DataXException;
 import com.alibaba.datax.common.plugin.BasicDataReceiver;
 import com.alibaba.datax.common.plugin.RecordReceiver;
+import com.alibaba.datax.common.plugin.TaskPluginCollector;
 import com.alibaba.datax.common.spi.Writer;
 import com.alibaba.datax.common.util.Configuration;
+import com.alibaba.datax.core.statistics.plugin.task.util.DirtyRecord;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.webank.wedatasphere.exchangis.datax.common.CryptoUtils;
 import com.webank.wedatasphere.exchangis.datax.plugin.writer.elasticsearchwriter.v6.column.ElasticColumn;
+import com.webank.wedatasphere.exchangis.datax.plugin.writer.elasticsearchwriter.v6.column.ElasticFieldDataType;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.entity.BufferedHttpEntity;
 import org.apache.http.entity.ContentType;
 import org.apache.http.nio.entity.NStringEntity;
+import org.elasticsearch.action.DocWriteRequest;
+import org.elasticsearch.action.bulk.BulkItemResponse;
+import org.elasticsearch.action.bulk.BulkProcessor;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.support.WriteRequest;
 import org.elasticsearch.client.Response;
 import org.elasticsearch.client.RestClient;
 import org.slf4j.Logger;
@@ -23,8 +34,7 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static com.google.common.base.Preconditions.checkArgument;
 import static com.webank.wedatasphere.exchangis.datax.plugin.writer.elasticsearchwriter.v6.ElasticRestWriter.Job.DEFAULT_ENDPOINT_SPLIT;
@@ -47,12 +57,115 @@ public class ElasticRestWriter  extends Writer {
 
         private Configuration jobConf = null;
         private String[] endPoints;
+        private String userName;
+        private String password;
 
         @Override
         public void prepare() {
             String indexName = this.jobConf.getNecessaryValue(ElasticKey.INDEX_NAME, ElasticWriterErrorCode.REQUIRE_VALUE);
             String indexType = this.jobConf.getString(ElasticKey.INDEX_TYPE, "_doc");
             log.info(String.format("index:[%s], type:[%s]", indexName, indexType));
+
+            //检查索引，创建索引，可以根据实际情况修改
+            ElasticRestHighClient restClient;
+            Map<String, Object> clientConfig = jobConf.getMap(ElasticKey.CLIENT_CONFIG);
+            if(StringUtils.isNotBlank(userName) && StringUtils.isNotBlank(password)){
+                restClient = ElasticRestHighClient.custom(endPoints, userName,
+                        password, clientConfig);
+            }else{
+                restClient = ElasticRestHighClient.custom(endPoints, clientConfig);
+            }
+            String columnNameSeparator = this.jobConf.getString(ElasticKey.COLUMN_NAME_SEPARATOR, ElasticColumn.DEFAULT_NAME_SPLIT);
+            List<Object> rawColumnList = jobConf
+                    .getList(ElasticKey.PROPS_COLUMN);
+            List<ElasticColumn> resolvedColumnList = new ArrayList<>();
+            Map<Object, Object> props = resolveColumn(restClient, indexName, indexType,
+                    rawColumnList, resolvedColumnList, columnNameSeparator);
+            this.jobConf.set(ElasticKey.PROPS_COLUMN, resolvedColumnList);
+            //clean up
+            if(jobConf.getBool(ElasticKey.CLEANUP, false) &&
+                    restClient.existIndices(indexName)){
+                if(!restClient.deleteIndices(indexName)){
+                    throw DataXException.asDataXException(ElasticWriterErrorCode.DELETE_INDEX_ERROR, "cannot delete index:[" + indexName +"]");
+                }
+            }
+
+            //if the index is not existed, create it
+            restClient.createIndex(indexName, indexType, jobConf.getMap(ElasticKey.SETTINGS),
+                    props);
+            restClient.close();
+        }
+
+        private Map<Object, Object> resolveColumn(ElasticRestHighClient client,
+                                                  String index, String type ,
+                                                  List<Object> rawColumnList, List<ElasticColumn> outputColumn,
+                                                  String columnNameSeparator){
+            Map<Object, Object> properties;
+            if(null != rawColumnList && !rawColumnList.isEmpty()) {
+                //allow to custom the fields of properties
+                properties = new HashMap<>(rawColumnList.size());
+                rawColumnList.forEach(columnRaw -> {
+                    String raw = columnRaw.toString();
+                    ElasticColumn column = JSONObject
+                            .parseObject(raw, ElasticColumn.class);
+                    if (StringUtils.isNotBlank(column.getName()) && StringUtils.isNotBlank(column.getType())) {
+                        outputColumn.add(column);
+                        if (!column.getName().equals(DEFAULT_ID) && ElasticFieldDataType.valueOf(column.getType().toUpperCase())
+                                != ElasticFieldDataType.ALIAS) {
+                            Map property = JSONObject.parseObject(raw, Map.class);
+                            property.remove(ElasticKey.PROPS_COLUMN_NAME);
+                            properties.put(column.getName(), property);
+                        }
+                    }
+                });
+            }else{
+                if(!client.existIndices(index)){
+                    throw DataXException.asDataXException(ElasticWriterErrorCode.INDEX_NOT_EXIST,
+                            "cannot get columns from index:[" + index +"]");
+                }
+                //get properties from index existed
+                properties = client.getProps(index, type);
+                resolveColumn(outputColumn, null, properties, columnNameSeparator);
+                //Reverse outputColumn
+                Collections.reverse(outputColumn);
+            }
+            return properties;
+        }
+
+        private void resolveColumn(List<ElasticColumn> outputColumn, ElasticColumn column,
+                                   Map<Object, Object> propsMap, String columnNameSeparator){
+            propsMap.forEach((key, value) ->{
+                if(value instanceof Map){
+                    Map metaMap = (Map)value;
+                    if(null != metaMap.get(ElasticKey.PROPS_COLUMN_TYPE)){
+                        ElasticColumn levelColumn = new ElasticColumn();
+                        if(null != column) {
+                            levelColumn.setName(column.getName() + columnNameSeparator + key);
+                        }else{
+                            levelColumn.setName(String.valueOf(key));
+                        }
+                        levelColumn.setType(String.valueOf(metaMap.get(ElasticKey.PROPS_COLUMN_TYPE)));
+                        if(null != metaMap.get(ElasticKey.PROPS_COLUMN_TIMEZONE)){
+                            levelColumn.setTimezone(String.valueOf(metaMap.get(ElasticKey.PROPS_COLUMN_TIMEZONE)));
+                        }
+                        if(null != metaMap.get(ElasticKey.PROPS_COLUMN_FORMAT)){
+                            levelColumn.setFormat(String.valueOf(metaMap.get(ElasticKey.PROPS_COLUMN_FORMAT)));
+                        }
+                        outputColumn.add(levelColumn);
+                    }else if(null != metaMap.get(ElasticRestHighClient.FIELD_PROPS)
+                            && metaMap.get(ElasticRestHighClient.FIELD_PROPS) instanceof Map){
+                        ElasticColumn levelColumn = column;
+                        if(null == levelColumn){
+                            levelColumn = new ElasticColumn();
+                            levelColumn.setName(String.valueOf(key));
+                        }else{
+                            levelColumn.setName(levelColumn.getName() + columnNameSeparator + key);
+                        }
+                        resolveColumn(outputColumn, levelColumn, (Map)metaMap.get(ElasticRestHighClient.FIELD_PROPS),
+                                columnNameSeparator);
+                    }
+                }
+            });
         }
 
         @Override
@@ -81,6 +194,16 @@ public class ElasticRestWriter  extends Writer {
                 throw DataXException.asDataXException(ElasticWriterErrorCode.REQUIRE_VALUE, "'endPoints(elasticUrls)' is necessary");
             }
 
+            this.userName = this.jobConf.getString(ElasticKey.USERNAME, "");
+            this.password = this.jobConf.getString(ElasticKey.PASSWORD, "");
+            if(StringUtils.isNotBlank(this.password)){
+                try {
+                    this.password = (String) CryptoUtils.string2Object(this.password);
+                } catch (Exception e) {
+                    throw DataXException.asDataXException(ElasticWriterErrorCode.CONFIG_ERROR, "decrypt password failed");
+                }
+            }
+
             this.endPoints = endPoints.split(DEFAULT_ENDPOINT_SPLIT);
             this.jobConf.getNecessaryValue(ElasticKey.INDEX_NAME, ElasticWriterErrorCode.REQUIRE_VALUE);
         }
@@ -103,14 +226,33 @@ public class ElasticRestWriter  extends Writer {
         private Long maxBatchSizeBytes = 5L * 1024L * 1024L;
         private int backendVersion;
 
+        private ElasticRestHighClient restHighClient;
+        private BulkProcessor bulkProcessor;
+        private volatile boolean bulkError;
+
         private static final ObjectMapper mapper = new ObjectMapper();
 
         @Override
         public void startWrite(BasicDataReceiver<Object> receiver, Class<?> type) {
-            logger.info("Begin to BasicDataReceiver write record to ElasticSearch");
-            super.startWrite(receiver, type);
-            logger.info("End to BasicDataReceiver write record to ElasticSearch");
-            throw DataXException.asDataXException(ElasticWriterErrorCode.FEATURES_ERROR, "功能不支持");
+            if(type.equals(DocWriteRequest.class)){
+                logger.info("Begin to write record to ElasticSearch");
+                long count = 0;
+                DocWriteRequest request = null;
+                while(null != (request = (DocWriteRequest) receiver.getFromReader())){
+                    request.index(indexName);
+                    request.type(typeName);
+                    if(bulkError){
+                        throw DataXException.asDataXException(ElasticWriterErrorCode.BULK_REQ_ERROR, "");
+                    }
+                    this.bulkProcessor.add(request);
+                    count += 1;
+                }
+                this.bulkProcessor.close();
+                getTaskPluginCollector().collectMessage(ElasticWriter.Job.WRITE_SIZE, String.valueOf(count));
+                logger.info("End to write record to ElasticSearch");
+            }else{
+                super.startWrite(receiver, type);
+            }
         }
         @Override
         public void startWrite(RecordReceiver lineReceiver) {
@@ -189,6 +331,27 @@ public class ElasticRestWriter  extends Writer {
             restClient = ElasticRestClient.createClient(endPoints);
 
             backendVersion = getBackendVersion();
+
+            //创建索引映射
+            int bulkPerTask = this.taskConf.getInt(ElasticKey.BULK_PER_TASK, 1);
+            String userName = this.taskConf.getString(ElasticKey.USERNAME, "");
+            String password = this.taskConf.getString(ElasticKey.PASSWORD, "");
+            if(StringUtils.isNotBlank(password)){
+                try {
+                    password = (String) CryptoUtils.string2Object(password);
+                } catch (Exception e) {
+                    throw DataXException.asDataXException(ElasticWriterErrorCode.CONFIG_ERROR, "decrypt password failed");
+                }
+            }
+
+            if(StringUtils.isNotBlank(userName) && StringUtils.isNotBlank(password)){
+                restHighClient = ElasticRestHighClient.custom(endPoints, userName,
+                        password, this.taskConf.getMap(ElasticKey.CLIENT_CONFIG));
+            }else{
+                restHighClient = ElasticRestHighClient.custom(endPoints, this.taskConf.getMap(ElasticKey.CLIENT_CONFIG));
+            }
+            this.bulkProcessor = restHighClient.createBulk(buildListener(getTaskPluginCollector()), batchSize, bulkPerTask);
+
         }
 
         @Override
@@ -199,6 +362,9 @@ public class ElasticRestWriter  extends Writer {
                 } catch (IOException e) {
                     throw DataXException.asDataXException(ElasticWriterErrorCode.CLOSE_EXCEPTION, e);
                 }
+            }
+            if(null != restHighClient){
+                restHighClient.close();
             }
         }
 
@@ -259,6 +425,39 @@ public class ElasticRestWriter  extends Writer {
 
         private static  JsonNode parseResponse(HttpEntity responseEntity) throws IOException {
             return mapper.readValue(responseEntity.getContent(), JsonNode.class);
+        }
+
+        private BulkProcessor.Listener buildListener(final TaskPluginCollector pluginCollector){
+            return new BulkProcessor.Listener() {
+                @Override
+                public void beforeBulk(long l, BulkRequest bulkRequest) {
+                    bulkRequest.setRefreshPolicy(WriteRequest.RefreshPolicy.NONE);
+                    logger.trace("do_bulk: " + bulkRequest.getDescription());
+                }
+
+                @Override
+                public void afterBulk(long l, BulkRequest bulkRequest, BulkResponse bulkResponse) {
+                    BulkItemResponse[] response = bulkResponse.getItems();
+                    for (BulkItemResponse itemResponse : response) {
+                        if (itemResponse.isFailed()) {
+                            List<String> message = new ArrayList<>();
+                            message.add(String.valueOf(itemResponse.getFailure().getStatus().getStatus()));
+                            message.add(itemResponse.getId());
+                            message.add(itemResponse.getFailureMessage());
+                            pluginCollector.collectDirtyRecord(new DirtyRecord(), null, JSON.toJSONString(message));
+                        }
+                    }
+                }
+
+                @Override
+                public void afterBulk(long l, BulkRequest bulkRequest, Throwable throwable) {
+                    //Ignore interrupted error
+                    if(!(throwable instanceof  InterruptedException)){
+                        logger.error(throwable.getMessage(), throwable);
+                    }
+                    bulkError = true;
+                }
+            };
         }
 
     }
