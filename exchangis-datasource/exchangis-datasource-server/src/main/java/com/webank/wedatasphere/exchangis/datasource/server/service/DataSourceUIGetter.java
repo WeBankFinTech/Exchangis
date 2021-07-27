b44383c3284573a4438169edaf2b34dcb4b317cd
nuts.jian <nuts.jian@gmail.com>
2021-07-27 13:19:44 +0800

    add datasource server

---
+++ exchangis-datasource/exchangis-datasource-server/src/main/java/com/webank/wedatasphere/exchangis/datasource/server/service/DataSourceUIGetter.java
@@ -0,0 +1,9 @@
+package com.webank.wedatasphere.exchangis.datasource.server.service;
+
+//import com.webank.wedatasphere.exchangis.datasource.core.ui.viewer.ExchangisDataSourceUIViewer;
+//
+//public interface DataSourceUIGetter {
+//
+//    ExchangisDataSourceUIViewer getDataSourceUI(String type);
+//
+//}
