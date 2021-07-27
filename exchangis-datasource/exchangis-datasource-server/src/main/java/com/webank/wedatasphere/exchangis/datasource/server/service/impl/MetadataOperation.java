d6c04410ab4d92cbf27cb2e49b79218842684766
nuts.jian <nuts.jian@gmail.com>
2021-07-27 13:19:44 +0800

    add datasource server

---
+++ exchangis-datasource/exchangis-datasource-server/src/main/java/com/webank/wedatasphere/exchangis/datasource/server/service/impl/MetadataOperation.java
@@ -0,0 +1,6 @@
+package com.webank.wedatasphere.exchangis.datasource.server.service.impl;
+
+import com.webank.wedatasphere.exchangis.datasource.server.service.MetadataServiceDispatcher;
+
+public class MetadataOperation<T> implements MetadataServiceDispatcher {
+}
