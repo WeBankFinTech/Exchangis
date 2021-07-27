f82861b6a0c9639e5ad437578d7d5b1b3f924a66
nuts.jian <nuts.jian@gmail.com>
2021-07-27 13:19:44 +0800

    add datasource server

---
+++ exchangis-datasource/exchangis-datasource-server/src/main/java/com/webank/wedatasphere/exchangis/datasource/server/loader/LocalExchangisDataSourceLoader.java
@@ -0,0 +1,35 @@
+package com.webank.wedatasphere.exchangis.datasource.server.loader;//package com.webank.wedatasphere.exchangis.datasource.server.loader;
+//
+//import com.webank.wedatasphere.exchangis.datasource.core.ExchangisDataSource;
+//import com.webank.wedatasphere.exchangis.datasource.core.context.ExchangisDataSourceContext;
+//import com.webank.wedatasphere.exchangis.datasource.core.loader.ExchangisDataSourceLoader;
+//import org.springframework.context.SmartLifecycle;
+//
+//import javax.annotation.PostConstruct;
+//
+//public class LocalExchangisDataSourceLoader implements ExchangisDataSourceLoader {
+//    @Override
+//    public void setClassLoader(Class<?> classLoader) {
+//
+//    }
+//
+//    @Override
+//    public void setContext(ExchangisDataSourceContext context) {
+//
+//    }
+//
+//    @Override
+//    public void init() {
+//
+//    }
+//
+//    @Override
+//    public ExchangisDataSource load(String dataSourceType) {
+//        return null;
+//    }
+//
+//    @Override
+//    public ExchangisDataSource get(String dataSourceType, boolean reload) {
+//        return null;
+//    }
+//}
