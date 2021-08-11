74feb2f54d15a3e158833dfb822f6c95043f8f83
ryanqin01 <qinruiyang@chinatelecom.cn>
2021-08-11 16:23:45 +0800

    use single

---
+++ exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/service/ExchangisJobService.java
@@ -0,0 +1,16 @@
+package com.webank.wedatasphere.exchangis.job.server.service;
+
+import com.webank.wedatasphere.exchangis.job.server.domain.ExchangisJob;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author yuxin.yuan
+ * @since 2021-08-10
+ */
+public interface ExchangisJobService extends IService<ExchangisJob> {
+
+}
