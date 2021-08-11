6f94863166d8500a1a287d34b20f0c0029cd233a
ryanqin01 <qinruiyang@chinatelecom.cn>
2021-08-11 16:23:45 +0800

    use single

---
+++ exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/mapper/ExchangisJobMapper.java
@@ -0,0 +1,16 @@
+package com.webank.wedatasphere.exchangis.job.server.mapper;
+
+import com.webank.wedatasphere.exchangis.job.server.domain.ExchangisJob;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author yuxin.yuan
+ * @since 2021-08-10
+ */
+public interface ExchangisJobMapper extends BaseMapper<ExchangisJob> {
+
+}
---
+++ exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/service/impl/ExchangisJobServiceImpl.java
@@ -0,0 +1,20 @@
+package com.webank.wedatasphere.exchangis.job.server.service.impl;
+
+import com.webank.wedatasphere.exchangis.job.server.domain.ExchangisJob;
+import com.webank.wedatasphere.exchangis.job.server.mapper.ExchangisJobMapper;
+import com.webank.wedatasphere.exchangis.job.server.service.ExchangisJobService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author yuxin.yuan
+ * @since 2021-08-10
+ */
+@Service
+public class ExchangisJobServiceImpl extends ServiceImpl<ExchangisJobMapper, ExchangisJob> implements ExchangisJobService {
+
+}
