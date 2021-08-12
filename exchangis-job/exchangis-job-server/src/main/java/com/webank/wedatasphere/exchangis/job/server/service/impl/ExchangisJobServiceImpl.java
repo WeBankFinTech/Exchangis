430a02cf9b39ec6c3fd8d37824fde586c704283e
yuxin.yuan <yuanyuxin@chinatelecom.cn>
2021-08-12 16:58:53 +0800

    实现新建任务接口

--- exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/service/impl/ExchangisJobServiceImpl.java
+++ exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/service/impl/ExchangisJobServiceImpl.java
@@ -1,10 +1,15 @@
 package com.webank.wedatasphere.exchangis.job.server.service.impl;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.webank.wedatasphere.exchangis.job.server.domain.ExchangisJob;
+import com.webank.wedatasphere.exchangis.job.server.dto.ExchangisJobBasicInfoDTO;
 import com.webank.wedatasphere.exchangis.job.server.mapper.ExchangisJobMapper;
 import com.webank.wedatasphere.exchangis.job.server.service.ExchangisJobService;
-import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import org.springframework.stereotype.Service;
+import com.webank.wedatasphere.exchangis.job.server.vo.ExchangisJobBasicInfoVO;
 
 /**
  * <p>
@@ -17,4 +22,16 @@ import org.springframework.stereotype.Service;
 @Service
 public class ExchangisJobServiceImpl extends ServiceImpl<ExchangisJobMapper, ExchangisJob> implements ExchangisJobService {
 
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private ExchangisJobService exchangisJobService;
+
+    @Override
+    public ExchangisJobBasicInfoVO createJob(ExchangisJobBasicInfoDTO exchangisJobBasicInfoDTO) {
+        ExchangisJob exchangisJob = modelMapper.map(exchangisJobBasicInfoDTO, ExchangisJob.class);
+        exchangisJobService.save(exchangisJob);
+        return modelMapper.map(exchangisJob, ExchangisJobBasicInfoVO.class);
+    }
 }
