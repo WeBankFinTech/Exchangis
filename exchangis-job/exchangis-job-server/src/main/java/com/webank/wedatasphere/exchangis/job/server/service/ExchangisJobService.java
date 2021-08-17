7b0a0abf476da5a24485ad28ec91799aaf5d46b0
ryanqin01 <qinruiyang@chinatelecom.cn>
2021-08-17 15:14:59 +0800

    主页面功能实现

--- exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/service/ExchangisJobService.java
+++ exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/service/ExchangisJobService.java
@@ -4,6 +4,9 @@ import com.baomidou.mybatisplus.extension.service.IService;
 import com.webank.wedatasphere.exchangis.job.server.domain.ExchangisJob;
 import com.webank.wedatasphere.exchangis.job.server.dto.ExchangisJobBasicInfoDTO;
 import com.webank.wedatasphere.exchangis.job.server.vo.ExchangisJobBasicInfoVO;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 /**
  * <p>
@@ -16,4 +19,16 @@ import com.webank.wedatasphere.exchangis.job.server.vo.ExchangisJobBasicInfoVO;
 public interface ExchangisJobService extends IService<ExchangisJob> {
 
     public ExchangisJobBasicInfoVO createJob(ExchangisJobBasicInfoDTO exchangisJobBasicInfoDTO);
+
+    public List<ExchangisJobBasicInfoVO> getJobList(long projectId, String type, String name);
+
+    public ExchangisJobBasicInfoVO copyJob(ExchangisJobBasicInfoDTO exchangisJobBasicInfoDTO, Long sourceJobId);
+
+    public ExchangisJobBasicInfoVO updateJob(ExchangisJobBasicInfoDTO exchangisJobBasicInfoDTO, Long id);
+
+    public ExchangisJobBasicInfoVO importSingleJob(MultipartFile multipartFile);
+
+    public void deleteJob(Long id);
+
+    public ExchangisJobBasicInfoVO getJob(Long id);
 }
