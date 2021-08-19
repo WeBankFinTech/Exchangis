4f76829c5aeae8348b26dafefd001993f92270bc
77954309@qq.com <Hkqo@9v738mUbvsP#>
2021-08-19 15:44:40 +0800

    updated version

--- exchangis-server/src/main/scala/com/webank/wedatasphere/exchangis/server/boot/ExchangisServerApplication.scala
+++ exchangis-server/src/main/scala/com/webank/wedatasphere/exchangis/server/boot/ExchangisServerApplication.scala
@@ -9,13 +9,7 @@ object ExchangisServerApplication {
   val hostName:String = Utils.getComputerName
 
   def main(args: Array[String]): Unit = {
-    //    val serviceName = System.getProperty("serviceName")//ProjectConf.SERVICE_NAME.getValue
-    //    DSSMainHelper.formatPropertyFiles(serviceName)
-    //    val allArgs = args ++ DSSMainHelper.getExtraSpringOptions
-    //    System.setProperty("hostName", hostName)
-    //    System.setProperty("userName", userName)
-    //    info(s"Ready to start $serviceName with args: ${allArgs.toList}.")
-    //    println(s"Test Ready to start $serviceName with args: ${allArgs.toList}.")
+
     DataWorkCloudApplication.main(args)
   }
 }
