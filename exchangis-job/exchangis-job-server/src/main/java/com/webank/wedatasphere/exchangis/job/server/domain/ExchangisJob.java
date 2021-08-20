a8bd9106c4690a8ac895b17ec110fcf6127d037d
ryanqin01 <qinruiyang@chinatelecom.cn>
2021-08-20 10:02:02 +0800

    add new features

--- exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/domain/ExchangisJob.java
+++ exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/domain/ExchangisJob.java
@@ -2,7 +2,10 @@ package com.webank.wedatasphere.exchangis.job.server.domain;
 
 import java.util.Date;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.webank.wedatasphere.exchangis.job.server.enums.JobTypeEnum;
 
 /**
  * <p>
@@ -17,6 +20,7 @@ public class ExchangisJob {
 
     private static final long serialVersionUID = 1L;
 
+    @TableId(type = IdType.AUTO)
     private Long id;
 
     private Long projectId;
@@ -199,7 +203,8 @@ public class ExchangisJob {
 
     @Override
     public String toString() {
-        return "ExchangisJob{" + "id=" + id + "projectId=" + projectId + ", jobName=" + jobName + ", jobType=" + jobType
+        return "ExchangisJob{" + "id=" + id + ", projectId=" + projectId + ", jobName=" + jobName + ", jobType="
+            + jobType
             + ", engineType=" + engineType + ", jobLabels=" + jobLabels + ", jobDesc=" + jobDesc + ", content="
             + content + ", alarmUser=" + alarmUser + ", alarmLevel=" + alarmLevel + ", proxyUser=" + proxyUser
             + ", executeNode=" + executeNode + ", syncType=" + syncType + ", jobParams=" + jobParams + ", createTime="
