453df4c29dc6de8a8f9e0f5592637bd29d8654a4
77954309@qq.com <Hkqo@9v738mUbvsP#>
2021-08-20 16:37:58 +0800

    dependency conflict

--- exchangis-server/src/main/scala/com/webank/wedatasphere/exchangis/server/boot/ExchangisServerApplication.scala
+++ exchangis-server/src/main/scala/com/webank/wedatasphere/exchangis/server/boot/ExchangisServerApplication.scala
@@ -1,12 +1,8 @@
 package com.webank.wedatasphere.exchangis.server.boot
 
 import com.webank.wedatasphere.linkis.DataWorkCloudApplication
-import com.webank.wedatasphere.linkis.common.utils.Utils
 
 object ExchangisServerApplication {
-
-  val userName:String = System.getProperty("username.name")
-  val hostName:String = Utils.getComputerName
 
   def main(args: Array[String]): Unit = {
 
