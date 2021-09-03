edff574ea5ce88dbbe9bf5d2dccc1d9193e22d98
nuts.jian <nuts.jian@gmail.com>
2021-09-03 15:42:59 +0800

    feat: datasource restapis

--- exchangis-datasource/exchangis-datasource-service/src/main/scala/com/webank/wedatasphere/exchangis/datasource/PublishDataSourceVersionAction.scala
+++ exchangis-datasource/exchangis-datasource-service/src/main/scala/com/webank/wedatasphere/exchangis/datasource/PublishDataSourceVersionAction.scala
@@ -3,7 +3,7 @@ package com.webank.wedatasphere.exchangis.datasource
 import com.webank.wedatasphere.linkis.datasource.client.request.DataSourceAction
 import com.webank.wedatasphere.linkis.httpclient.request.POSTAction
 
-class PublishDataSourceVersionAction(resourceId: Long) extends POSTAction with DataSourceAction {
+class PublishDataSourceVersionAction(resourceId: Long, versionId: Long) extends POSTAction with DataSourceAction {
   override def getRequestPayload: String = ""
 
   private var user: String = _
@@ -12,5 +12,6 @@ class PublishDataSourceVersionAction(resourceId: Long) extends POSTAction with D
 
   override def getUser: String = this.user
 
-  override def suffixURLs: Array[String] = Array("datasourcemanager", "info", resourceId+"", "version")
+//  override def suffixURLs: Array[String] = Array("datasourcemanager", "info", resourceId+"", "version")
+  override def suffixURLs: Array[String] = Array("datasourcemanager", "publish", resourceId+"", versionId+"")
 }
