3960e62f2e6b03948e6666f49e655b203bc48983
liangx <46127018+15100399015@users.noreply.github.com>
2021-09-04 14:49:27 +0800

    前端提交

--- web/src/common/utils.js
+++ web/src/common/utils.js
@@ -1,42 +1,48 @@
-import { cloneDeep, isNil, isInteger } from 'lodash-es';
-import { BASE_URL } from './constants';
-
+import { cloneDeep, isNil, isInteger } from "lodash-es";
+import moment from "moment";
+import { BASE_URL } from "./constants";
 
 export const arrToObj = (arr, key) => {
-    if (!Array.isArray(arr)) return null;
-    if (!key) {
-        console.error('arrToObj: key is required');
-        return null;
-    }
-    return arr.reduce((acc, cur) => {
-        acc[cur[key]] = cur;
-        return acc;
-    }, {});
+  if (!Array.isArray(arr)) return null;
+  if (!key) {
+    console.error("arrToObj: key is required");
+    return null;
+  }
+  return arr.reduce((acc, cur) => {
+    acc[cur[key]] = cur;
+    return acc;
+  }, {});
 };
 
 export const extractionObj = (obj, standard) => {
-    const cloneObj = cloneDeep(obj);
-    const cloneStandard = cloneDeep(standard);
-    Object.key(standard).forEach((key) => {
-        if (!isNil(cloneObj[key])) {
-            cloneStandard[key] = cloneObj[key];
-        }
-    });
-    return cloneStandard;
+  const cloneObj = cloneDeep(obj);
+  const cloneStandard = cloneDeep(standard);
+  Object.key(standard).forEach((key) => {
+    if (!isNil(cloneObj[key])) {
+      cloneStandard[key] = cloneObj[key];
+    }
+  });
+  return cloneStandard;
 };
 
-export const isPositiveInteger = num => isInteger(num) && num > 0;
+export const isPositiveInteger = (num) => isInteger(num) && num > 0;
 
-export const nonNegativeInteger = num => isInteger(num) && num >= 0;
+export const nonNegativeInteger = (num) => isInteger(num) && num >= 0;
 
 export function getQueryVariable(variable) {
-    const query = window.location.search.substring(1);
-    const vars = query.split('&');
-    for (let i = 0; i < vars.length; i++) {
-        const pair = vars[i].split('=');
-        if (pair[0] === variable) { return pair[1]; }
+  const query = window.location.search.substring(1);
+  const vars = query.split("&");
+  for (let i = 0; i < vars.length; i++) {
+    const pair = vars[i].split("=");
+    if (pair[0] === variable) {
+      return pair[1];
     }
-    return (false);
+  }
+  return false;
 }
 
 export const genFileURL = (fileId, fileHash) => `${BASE_URL}/record/download/file/${fileId}/${fileHash}`;
+
+export const dateFormat = (timestamp) => {
+  return moment(timestamp).format("YYYY-MM-DD HH:mm");
+};
