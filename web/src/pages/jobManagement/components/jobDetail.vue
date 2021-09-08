db369f46e2e11dc341934443caeb370ca4098dea
汪鸣峰 <wangmingfeng@chelun.com>
2021-09-08 09:44:22 +0800

    update

--- web/src/pages/jobManagement/components/jobDetail.vue
+++ web/src/pages/jobManagement/components/jobDetail.vue
@@ -1,9 +1,26 @@
 <template>
     <div class="container">
-        <SettingOutlined />配置
-        <CaretRightOutlined />执行
-        <SaveOutlined />保存
-        <HistoryOutlined />执行历史
+        <div class="tools-bar">
+          <span @click="modalCfg.visible=true"><SettingOutlined />配置</span>
+          <span><CaretRightOutlined />执行</span>
+          <span><SaveOutlined />保存</span>
+          <span><HistoryOutlined />执行历史</span>
+        </div>
+        <div class="jd-content">
+          <div class="jd_left">
+            <DatabaseFilled /><span>子任务列表</span><PlusSquareOutlined />
+            <div>
+              <div>任务1<CopyOutlined /> <DeleteOutlined /></div>
+              <div>xxxxxx</div>
+              <div> <ArrowDownOutlined /> </div>
+              <div>xxxxxx</div>
+            </div>
+          </div>
+          <div class="jd_right">
+            <CheckCircleOutlined />数据源
+          </div>
+        </div>
+      <config-modal v-model:visible="modalCfg.visible" :id="modalCfg.id" @finish="handleModalFinish" />
     </div>
 </template>
 <script>
@@ -12,18 +29,36 @@
     SettingOutlined,
     CaretRightOutlined,
     SaveOutlined,
-    HistoryOutlined
+    HistoryOutlined,
+    DatabaseFilled,
+    PlusSquareOutlined,
+    CopyOutlined,
+    DeleteOutlined,
+    ArrowDownOutlined,
+    CheckCircleOutlined
   } from "@ant-design/icons-vue";
+  import configModal from './configModal'
   export default {
     components: {
       SettingOutlined,
       CaretRightOutlined,
       SaveOutlined,
-      HistoryOutlined
+      HistoryOutlined,
+      DatabaseFilled,
+      PlusSquareOutlined,
+      CopyOutlined,
+      DeleteOutlined,
+      ArrowDownOutlined,
+      CheckCircleOutlined,
+      configModal
     },
     data() {
       return {
-        name: ''
+        name: '',
+        modalCfg: {
+          id: '',
+          visible: false,
+        }
       };
     },
     props: {
@@ -41,7 +76,23 @@
       init(){
         this.name = this.curTab.jobName
         console.log(this.curTab, this.name)
+      },
+      handleModalFinish(){
+
       }
     }
   }
 </script>
+<style scoped lang="less">
+.container {
+    .jd-content{
+        display: flex;
+        .jd_left{
+          flex: 1;
+        }
+        .jd_right{
+          flex: 4;
+        }
+    }
+}
+</style>
