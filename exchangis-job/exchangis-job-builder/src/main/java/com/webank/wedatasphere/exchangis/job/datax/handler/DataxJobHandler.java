073bc81fd782c167a79032b5a8ae837011bcab45
ryanqin01 <qinruiyang@chinatelecom.cn>
2021-09-08 16:19:31 +0800

    优化job builder。实现完整的mysqlreader和mysqlwriter

--- exchangis-job/exchangis-job-builder/src/main/java/com/webank/wedatasphere/exchangis/job/datax/handler/DataxJobHandler.java
+++ exchangis-job/exchangis-job-builder/src/main/java/com/webank/wedatasphere/exchangis/job/datax/handler/DataxJobHandler.java
@@ -1,7 +1,17 @@
 package com.webank.wedatasphere.exchangis.job.datax.handler;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.webank.wedatasphere.exchangis.datasource.service.ExchangisDataSourceService;
 import com.webank.wedatasphere.exchangis.job.handler.JobHandler;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public abstract class DataxJobHandler implements JobHandler {
+
+    @Autowired
+    ExchangisDataSourceService dataSourceServiceservice;
+
+    GsonBuilder builder = new GsonBuilder();
+    Gson gson = builder.create();
 
 }
