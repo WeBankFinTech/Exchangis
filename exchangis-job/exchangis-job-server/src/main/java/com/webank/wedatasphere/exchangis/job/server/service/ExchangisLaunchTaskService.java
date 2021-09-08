a6788267fba0870aac92799ccd0017ac5f0c0abd
yuxin.yuan <yuanyuxin@chinatelecom.cn>
2021-09-08 16:55:24 +0800

    add exchangis launch task operations

---
+++ exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/service/ExchangisLaunchTaskService.java
@@ -0,0 +1,6 @@
+package com.webank.wedatasphere.exchangis.job.server.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.webank.wedatasphere.exchangis.job.domain.ExchangisLaunchTask;
+
+public interface ExchangisLaunchTaskService extends IService<ExchangisLaunchTask> {}
