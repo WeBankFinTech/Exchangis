1ef852e5196a9e0fe9f6bb27f779dc42f604e63b
汪鸣峰 <wangmingfeng@chelun.com>
2021-09-08 15:25:29 +0800

    增加复制任务

---
+++ web/src/pages/jobManagement/components/copyModal.vue
@@ -0,0 +1,97 @@
+<template>
+  <a-modal title="复制任务" :visible="visible" :confirm-loading="confirmLoading" @ok="handleOk" @cancel="$emit('update:visible', false)">
+    <a-form ref="formRef" :rules="rules" :model="formState" :label-col="{ span: 4 }">
+      <a-form-item label="原任务" name="jobNameOrigin">
+        <a-input v-model:value="origin.jobName" disabled/>
+      </a-form-item>
+      <a-form-item label="任务名" name="jobName">
+        <a-input v-model:value="formState.jobName" />
+      </a-form-item>
+      <a-form-item label="业务标签" name="jobLabels">
+        <a-input v-model:value="formState.jobLabels" />
+      </a-form-item>
+      <a-form-item label="执行引擎" name="engineType">
+        <a-input v-model:value="origin.engineType" disabled/>
+      </a-form-item>
+      <a-form-item label="任务描述" name="jobDesc">
+        <a-input v-model:value="formState.jobDesc" />
+      </a-form-item>
+    </a-form>
+  </a-modal>
+</template>
+
+<script>
+import { toRaw } from "vue";
+import { message } from "ant-design-vue";
+import { copyJob } from "@/common/service";
+import {
+  PlusOutlined,
+} from "@ant-design/icons-vue";
+import { createProject, getProjectById, updateProject } from "@/common/service";
+export default {
+  name: "JobManagementConfigModal",
+  components: {
+    PlusOutlined
+  },
+  props: {
+    // 是否可见
+    visible: {
+      type: Boolean,
+      required: true,
+    },
+    origin: {
+      type: Object,
+      default: {}
+    }
+  },
+  emits: ["finish", "cancel", "update:visible"],
+  data() {
+    return {
+      // 是否加载中
+      confirmLoading: false,
+      // 表单数据
+      formState: { jobName: '',
+        jobLabels: '',
+        jobDesc: ''
+      },
+      // 验证
+      rules: {
+        jobName: [{ required: true}]
+      },
+    };
+  },
+  watch: {
+  },
+  methods: {
+    async handleOk() {
+      await this.$refs.formRef.validate();
+      const formatData = {
+        ...this.formState
+      };
+      try {
+        this.confirmLoading = true;
+        await copyJob(this.origin.id ,formatData);
+        message.success("复制成功");
+      } catch (error) {}
+      this.confirmLoading = false;
+      this.$emit("update:visible", false);
+      this.$emit("finish");
+    }
+  }
+};
+</script>
+
+<style scoped lang="less">
+  .fr{
+    float: right;
+  }
+  .fl {
+    float: left;
+  }
+  .w40{
+    width: 40%;
+  }
+  .separator {
+    margin: 5px;
+  }
+</style>
