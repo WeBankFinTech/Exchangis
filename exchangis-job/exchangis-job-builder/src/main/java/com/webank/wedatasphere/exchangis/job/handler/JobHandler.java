518c71297a75f13a4a9e7e1decb9571386353a57
ryanqin01 <qinruiyang@chinatelecom.cn>
2021-09-08 16:19:31 +0800

    优化job builder。实现完整的mysqlreader和mysqlwriter

--- exchangis-job/exchangis-job-builder/src/main/java/com/webank/wedatasphere/exchangis/job/handler/JobHandler.java
+++ exchangis-job/exchangis-job-builder/src/main/java/com/webank/wedatasphere/exchangis/job/handler/JobHandler.java
@@ -1,12 +1,13 @@
 package com.webank.wedatasphere.exchangis.job.handler;
 
+import com.webank.wedatasphere.exchangis.datasource.core.exception.ExchangisDataSourceException;
 import com.webank.wedatasphere.exchangis.job.datax.reader.Reader;
 import com.webank.wedatasphere.exchangis.job.datax.writer.Writer;
 import com.webank.wedatasphere.exchangis.job.domain.ExchangisSubJob;
 
 public interface JobHandler {
 
-    Reader handlerReader(ExchangisSubJob job);
+    Reader handlerReader(ExchangisSubJob job, Long jobId) throws ExchangisDataSourceException;
 
-    Writer handlerWriter(ExchangisSubJob job);
+    Writer handlerWriter(ExchangisSubJob job, Long jobId);
 }
