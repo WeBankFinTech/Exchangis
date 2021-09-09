bb5dfef79955a6357e06e236bfcf8fd839b58d28
汪鸣峰 <wangmingfeng@chelun.com>
2021-09-09 15:25:27 +0800

    增加mock

--- web/src/pages/jobManagement/components/jobDetail.vue
+++ web/src/pages/jobManagement/components/jobDetail.vue
@@ -1,32 +1,50 @@
 <template>
-    <div class="container">
-        <div class="tools-bar">
-          <span @click="modalCfg.visible=true"><SettingOutlined />配置</span>
-          <span><CaretRightOutlined />执行</span>
-          <span><SaveOutlined />保存</span>
-          <span><HistoryOutlined />执行历史</span>
-        </div>
-        <div class="jd-content">
-          <div class="jd_left">
-            <DatabaseFilled /><span>子任务列表</span><PlusSquareOutlined />
-            <div v-for="item in list">
-              <div>{{item.jobName}}<CopyOutlined @click="copySub(item)" /> <DeleteOutlined /></div>
-              <div>xxxxxx</div>
-              <div> <ArrowDownOutlined /> </div>
-              <div>xxxxxx</div>
-            </div>
-          </div>
-          <div class="jd_right">
-            <CheckCircleOutlined />数据源
-          </div>
-        </div>
-      <config-modal v-model:visible="modalCfg.visible" :id="modalCfg.id" @finish="handleModalFinish" />
-      <copy-modal v-model:visible="modalCopy.visible" :origin="copyObj" @finish="handleModalCopy" />
+  <div class="container">
+    <div class="tools-bar">
+      <span @click="modalCfg.visible=true"><SettingOutlined />配置</span>
+      <span><CaretRightOutlined />执行</span>
+      <span><SaveOutlined />保存</span>
+      <span><HistoryOutlined />执行历史</span>
     </div>
+    <div class="jd-content">
+      <div class="jd_left">
+        <DatabaseFilled /><span>子任务列表</span><PlusSquareOutlined />
+        <div v-for="item in list">
+          <div>{{item.subjobName}}<CopyOutlined @click="copySub(item)" /> <DeleteOutlined /></div>
+          <div>xxxxxx</div>
+          <div> <ArrowDownOutlined /> </div>
+          <div>xxxxxx</div>
+        </div>
+      </div>
+      <div class="jd_right">
+        <CheckCircleOutlined />数据源
+      </div>
+    </div>
+    <config-modal v-model:visible="modalCfg.visible" :id="modalCfg.id" @finish="handleModalFinish" />
+    <copy-modal v-model:visible="modalCopy.visible" :origin="copyObj" @finish="handleModalCopy" />
+  </div>
 </template>
 <script>
-  import { toRaw } from "vue";
-  import {
+import { toRaw } from "vue";
+import {
+  SettingOutlined,
+  CaretRightOutlined,
+  SaveOutlined,
+  HistoryOutlined,
+  DatabaseFilled,
+  PlusSquareOutlined,
+  CopyOutlined,
+  DeleteOutlined,
+  ArrowDownOutlined,
+  CheckCircleOutlined
+} from "@ant-design/icons-vue";
+import configModal from './configModal'
+import copyModal from './copyModal'
+import { getJobInfo } from "@/common/service";
+import { jobInfo } from "../mock"
+
+export default {
+  components: {
     SettingOutlined,
     CaretRightOutlined,
     SaveOutlined,
@@ -36,73 +54,64 @@
     CopyOutlined,
     DeleteOutlined,
     ArrowDownOutlined,
-    CheckCircleOutlined
-  } from "@ant-design/icons-vue";
-  import configModal from './configModal'
-  import copyModal from './copyModal'
-  export default {
-    components: {
-      SettingOutlined,
-      CaretRightOutlined,
-      SaveOutlined,
-      HistoryOutlined,
-      DatabaseFilled,
-      PlusSquareOutlined,
-      CopyOutlined,
-      DeleteOutlined,
-      ArrowDownOutlined,
-      CheckCircleOutlined,
-      configModal,
-      copyModal
-    },
-    data() {
-      return {
-        name: '',
-        modalCfg: {
-          id: '',
-          visible: false,
-        },
-        modalCopy: {
-          visible: false,
-        },
-        copyObj: {},
-        list:[
-          {
-            id: 1,
-            jobName: '任务1',
-            engineType: 'DataX'
-          }
-        ]
-      };
-    },
-    props: {
-      curTab: Object
-    },
-    watch:{
-      curTab(n, o){
-        this.init()
-      }
-    },
-    mounted() {
+    CheckCircleOutlined,
+    configModal,
+    copyModal
+  },
+  data() {
+    return {
+      name: '',
+      modalCfg: {
+        id: '',
+        visible: false,
+      },
+      modalCopy: {
+        visible: false,
+      },
+      jobData: {},
+      copyObj: {},
+      list:[]
+    };
+  },
+  props: {
+    curTab: Object
+  },
+  watch:{
+    curTab(n, o){
       this.init()
+    }
+  },
+  mounted() {
+    this.init()
+  },
+  methods: {
+    init(){
+      this.name = this.curTab.jobName
+      this.getInfo()
     },
-    methods: {
-      init(){
-        this.name = this.curTab.jobName
-        console.log(this.curTab, this.name)
-      },
-      handleModalFinish(){
+    async getInfo() {
+      try {
+        //await getJobInfo(this.curTab.id);
+        jobInfo.content.subJobs.forEach(item => {
+          item.engineType = jobInfo.engineType
+        })
+        this.jobData = jobInfo
+        this.list = this.jobData.content.subJobs
+        console.log(this.jobData)
+      } catch (error) {}
+    },
+    handleModalFinish(){
 
-      },
-      handleModalCopy() {
+    },
+    handleModalCopy() {
 
-      },
-      copySub(item) {
-        this.copyObj = item
-        this.modalCopy.visible = true
-      }
+    },
+    copySub(item) {
+      this.copyObj = item
+      this.modalCopy.visible = true
     }
   }
+}
 </script>
 <style scoped lang="less">
 .container {
