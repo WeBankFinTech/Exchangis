7f618a0f05ad74a61f1ec1e7f7cbe9e15ca57b9c
汪鸣峰 <wangmingfeng@chelun.com>
2021-09-16 09:59:28 +0800

    refine style lint

--- web/src/pages/jobManagement/components/jobDetail.vue
+++ web/src/pages/jobManagement/components/jobDetail.vue
@@ -12,19 +12,20 @@
     <div class="jd-content">
       <div class="jd_left">
         <div class="sub-title">
-          <DatabaseFilled />
+          <DatabaseFilled class="database-icon"/>
           <span>子任务列表</span>
-          <PlusSquareOutlined />
+          <PlusSquareOutlined class="ps-icon"/>
         </div>
-        <div v-for="(item, idx) in list" :key="idx">
-          <div>
-            {{ item.subjobName }}<CopyOutlined @click="copySub(item)" />
-            <DeleteOutlined @click="deleteSub(idx)" />
+        <div v-for="(item, idx) in list" :key="idx" class="sub-content">
+          <div class="task-title">
+            <span class="subjobName">{{ item.subjobName }}</span>
+            <DeleteOutlined class="delete-icon" @click="deleteSub(idx)" />
+            <CopyOutlined class="copy-icon" @click="copySub(item)" />
           </div>
           <template v-if="item.dataSourceIds">
-            <div >{{ item.dataSourceIds.source.db + '.' +  item.dataSourceIds.source.table }}</div>
-            <div><ArrowDownOutlined /></div>
-            <div>{{ item.dataSourceIds.sink.db + '.' +  item.dataSourceIds.sink.table }}</div>
+            <div class="sub-table" :title="item.dataSourceIds.source.db + '.' +  item.dataSourceIds.source.table">{{ item.dataSourceIds.source.db + '.' +  item.dataSourceIds.source.table }}</div>
+            <div class="arrow-down-icon"><ArrowDownOutlined /></div>
+            <div class="sub-table" :title="item.dataSourceIds.sink.db + '.' +  item.dataSourceIds.sink.table">{{ item.dataSourceIds.sink.db + '.' +  item.dataSourceIds.sink.table }}</div>
           </template>
         </div>
       </div>
@@ -161,6 +162,7 @@ export default {
     padding: 10px 30px;
     font-size: 16px;
     >span {
+      cursor: pointer;
     }
     .anticon {
       margin-right: 5px;
@@ -178,17 +180,69 @@ export default {
   }
   .jd-content {
     display: flex;
+    overflow-x: auto;
+    width: 100%;
     .jd_left {
-      flex: 1;
-      padding-left: 25px;
+      flex: 2;
+      padding: 0 25px;
       .sub-title {
         font-size: 16px;
         font-weight: bolder;
         margin-top: 15px;
+        .database-icon {
+          color: rgb(102, 102, 255);
+          margin-right: 5px;
+        }
+        .ps-icon {
+          float: right;
+          cursor: pointer;
+          line-height: 28px;
+        }
+      }
+      .sub-content {
+        width: 200px;
+        margin: 10px 0;
+        border: 1px solid rgb(102, 102, 255);
+        padding: 10px 15px;
+        border-radius: 5px;
+        .task-title {
+          font-size: 15px;
+          font-weight: bolder;
+          .subjobName{
+            color: rgb(102, 102, 255);
+            cursor: pointer;
+          }
+          .delete-icon {
+            float: right;
+            cursor: pointer;
+            margin-right: 10px;
+            line-height: 28px;
+          }
+          .copy-icon {
+            float: right;
+            cursor: pointer;
+            margin-right: 5px;
+            line-height: 28px;
+          }
+        }
+        .sub-table {
+          color: rgb(22, 155, 213);
+          text-align: center;
+          margin: 5px 0;
+          width: 100%;
+          overflow: hidden;
+          white-space: nowrap;
+          text-overflow: ellipsis;
+        }
+        .arrow-down-icon {
+          text-align: center;
+          font-weight: bolder;
+          font-size: 16px;
+        }
       }
     }
     .jd_right {
-      flex: 4;
+      flex: 5;
     }
   }
 }
