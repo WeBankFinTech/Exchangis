e61e6004c0fe91e4fc862c1e59d0134d15c56dbf
ryanqin01 <qinruiyang@chinatelecom.cn>
2021-10-13 14:08:30 +0800

    增加获取执行子任务列表功能

--- exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/service/impl/ExchangisLaunchTaskServiceImpl.java
+++ exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/service/impl/ExchangisLaunchTaskServiceImpl.java
@@ -1,12 +1,41 @@
 package com.webank.wedatasphere.exchangis.job.server.service.impl;
 
-import org.springframework.stereotype.Service;
-
+import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.webank.wedatasphere.exchangis.job.domain.ExchangisLaunchTask;
 import com.webank.wedatasphere.exchangis.job.server.mapper.ExchangisLaunchTaskMapper;
 import com.webank.wedatasphere.exchangis.job.server.service.ExchangisLaunchTaskService;
+import com.webank.wedatasphere.exchangis.job.server.vo.ExchangisTaskInfoVO;
+import org.apache.commons.lang.StringUtils;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ExchangisLaunchTaskServiceImpl extends ServiceImpl<ExchangisLaunchTaskMapper, ExchangisLaunchTask>
-    implements ExchangisLaunchTaskService {}
+        implements ExchangisLaunchTaskService {
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private ExchangisLaunchTaskService exchangisLaunchTaskService;
+
+    @Override
+    public List<ExchangisTaskInfoVO> getTaskList(long taskId, String taskName) {
+        LambdaQueryChainWrapper<ExchangisLaunchTask> query =
+                exchangisLaunchTaskService.lambdaQuery().eq(ExchangisLaunchTask::getId, taskId);
+        if (StringUtils.isNotBlank(taskName)) {
+            query.like(ExchangisLaunchTask::getTaskName, taskName.trim());
+        }
+        List<ExchangisLaunchTask> exchangisLaunchTasks = query.list();
+
+        List<ExchangisTaskInfoVO> returnlist = new ArrayList<>();
+        exchangisLaunchTasks.stream().forEach(task -> returnlist.add(modelMapper.map(task, ExchangisTaskInfoVO.class)));
+
+        return returnlist;
+    }
+}
---
+++ exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/vo/ExchangisTaskInfoVO.java
@@ -0,0 +1,44 @@
+package com.webank.wedatasphere.exchangis.job.server.vo;
+
+public class ExchangisTaskInfoVO {
+
+    private Long id;
+
+    private String taskName;
+
+    private Long jobId;
+
+    private String jobName;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public Long getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(Long jobId) {
+        this.jobId = jobId;
+    }
+
+    public String getJobName() {
+        return jobName;
+    }
+
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+}
