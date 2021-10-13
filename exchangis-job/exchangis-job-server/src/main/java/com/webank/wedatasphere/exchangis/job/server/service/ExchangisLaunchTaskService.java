7d73aaaf9b44e2bfb772222a1c000f2d6d64ee72
ryanqin01 <qinruiyang@chinatelecom.cn>
2021-10-13 14:08:30 +0800

    增加获取执行子任务列表功能

--- exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/service/ExchangisLaunchTaskService.java
+++ exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/service/ExchangisLaunchTaskService.java
@@ -2,5 +2,18 @@ package com.webank.wedatasphere.exchangis.job.server.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.webank.wedatasphere.exchangis.job.domain.ExchangisLaunchTask;
+import com.webank.wedatasphere.exchangis.job.server.vo.ExchangisTaskInfoVO;
 
-public interface ExchangisLaunchTaskService extends IService<ExchangisLaunchTask> {}
+import java.util.List;
+
+public interface ExchangisLaunchTaskService extends IService<ExchangisLaunchTask> {
+
+    /**
+     * Gets task list(return task basic info).
+     *
+     * @param taskId   the task id
+     * @param taskName the task name
+     * @return the task list
+     */
+    List<ExchangisTaskInfoVO> getTaskList(long taskId, String taskName);
+}
