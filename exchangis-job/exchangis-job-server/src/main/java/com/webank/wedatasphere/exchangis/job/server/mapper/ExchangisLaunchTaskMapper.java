e622754d899b75701a04659d594602dd5d6b60de
yuxin.yuan <yuanyuxin@chinatelecom.cn>
2021-10-15 10:46:49 +0800

    task page interface

--- exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/mapper/ExchangisLaunchTaskMapper.java
+++ exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/mapper/ExchangisLaunchTaskMapper.java
@@ -1,5 +1,10 @@
 package com.webank.wedatasphere.exchangis.job.server.mapper;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.webank.wedatasphere.exchangis.job.domain.ExchangisLaunchTask;
 
@@ -9,4 +14,10 @@ import com.webank.wedatasphere.exchangis.job.domain.ExchangisLaunchTask;
  * @author yuxin.yuan
  * @since 2021-09-08
  */
-public interface ExchangisLaunchTaskMapper extends BaseMapper<ExchangisLaunchTask> {}
+@Mapper
+public interface ExchangisLaunchTaskMapper extends BaseMapper<ExchangisLaunchTask> {
+
+    List<ExchangisLaunchTask> getTaskList(@Param("taskId") Long taskId, @Param("taskName") String taskName,
+        @Param("status") String status, @Param("launchStartTime") Long launchStartTime,
+        @Param("launchEndTime") Long launchEndTime, @Param("start") int start, @Param("size") int size);
+}
