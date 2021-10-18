d4ea23b5dae45892fab674b9694c38a996baabe5
ryanqin01 <qinruiyang@chinatelecom.cn>
2021-10-18 17:08:42 +0800

    sqoop优化

--- exchangis-job/exchangis-job-builder/src/main/java/com/webank/wedatasphere/exchangis/job/sqoop/writer/SqoopWriter.java
+++ exchangis-job/exchangis-job-builder/src/main/java/com/webank/wedatasphere/exchangis/job/sqoop/writer/SqoopWriter.java
@@ -3,4 +3,14 @@ package com.webank.wedatasphere.exchangis.job.sqoop.writer;
 import com.webank.wedatasphere.exchangis.job.domain.Writer;
 
 public class SqoopWriter extends Writer {
+
+    private String writerString;
+
+    public String getWriterString() {
+        return writerString;
+    }
+
+    public void setWriterString(String writerString) {
+        this.writerString = writerString;
+    }
 }
