dd128cca3400e6f67d44a34d686a6840e546da33
Dlimeng <77954309@qq.com>
2021-11-08 16:45:22 +0800

    plugin

--- exchangis-plugins/exchangis-appconn/src/main/java/com/webank/wedatasphere/exchangis/appconn/config/ExchangisConfig.java
+++ exchangis-plugins/exchangis-appconn/src/main/java/com/webank/wedatasphere/exchangis/appconn/config/ExchangisConfig.java
@@ -5,7 +5,7 @@ import com.webank.wedatasphere.linkis.server.conf.ServerConfiguration;
 
 public class ExchangisConfig {
 
-    final static String baseUrl = "/api/rest_s/" + ServerConfiguration.BDP_SERVER_VERSION() + "/exchangis";
-    final static String EXCHANGIS_APPCONN_NAME = CommonVars.apply("wds.dss.appconn.exchangis.name", "Exchangis").getValue();
+    public final static String baseUrl = "/api/rest_s/" + ServerConfiguration.BDP_SERVER_VERSION() + "/exchangis";
+    public final static String EXCHANGIS_APPCONN_NAME = CommonVars.apply("wds.dss.appconn.exchangis.name", "Exchangis").getValue();
 
 }
