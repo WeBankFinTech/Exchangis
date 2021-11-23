44400b9f9daf2c8bfdba880ca01daba8ba8eeb04
nuts.jian <nuts.jian@gmail.com>
2021-11-23 14:39:39 +0800

    feat: exchangis metric init commit

---
+++ exchangis-job/exchangis-job-metrics/src/main/java/com/webank/wedatasphere/exchangis/metrics/impl/NormalMetricsCollector.java
@@ -0,0 +1,11 @@
+package com.webank.wedatasphere.exchangis.metrics.impl;
+
+import com.webank.wedatasphere.exchangis.metrics.api.Counter;
+import com.webank.wedatasphere.exchangis.metrics.api.MetricName;
+
+public class NormalMetricsCollector extends MetricsCollector {
+    @Override
+    public void collect(MetricName name, Counter counter, long timestamp) {
+
+    }
+}
