03038987abb1657486c60f4da31fb8d3a321947b
Davidhua1996 <david_hua1996@hotmail.com>
2021-11-29 00:08:18 +0800

    Add asembly package module  Append start stop scripts for modules

--- exchangis-server/src/main/scala/com/webank/wedatasphere/exchangis/server/boot/ExchangisServerApplication.scala
+++ exchangis-server/src/main/scala/com/webank/wedatasphere/exchangis/server/boot/ExchangisServerApplication.scala
@@ -1,11 +1,37 @@
 package com.webank.wedatasphere.exchangis.server.boot
 
-import com.webank.wedatasphere.linkis.DataWorkCloudApplication
 
+import com.webank.wedatasphere.linkis.DataWorkCloudApplication
+import com.webank.wedatasphere.linkis.common.utils.Utils
+import com.webank.wedatasphere.linkis.server.utils.LinkisMainHelper
+import org.apache.commons.lang.{ArrayUtils, StringUtils}
+import org.slf4j.{Logger, LoggerFactory}
+class ExchangisServerApplication{
+
+}
 object ExchangisServerApplication {
 
-  def main(args: Array[String]): Unit = {
+  private val LOG: Logger = LoggerFactory.getLogger(classOf[ExchangisServerApplication])
 
-    DataWorkCloudApplication.main(args)
+  private val MAIN_PROGRAM_NAME: String = "exchangis"
+
+  def main(args: Array[String]): Unit = {
+    LOG.info("Start to run ExchangisServerApplication")
+    ApplicationUtils.savePidAndRun{
+      val userName = Utils.getJvmUser
+      val hostName = Utils.getComputerName
+      System.setProperty("hostName", hostName)
+      System.setProperty("userName", userName)
+      val serviceName = Option(System.getProperty(LinkisMainHelper.SERVER_NAME_KEY)).getOrElse(MAIN_PROGRAM_NAME)
+      System.setProperty("spring.application.name", serviceName)
+      LinkisMainHelper.formatPropertyFiles(MAIN_PROGRAM_NAME, serviceName)
+      val allArgs: Array[String] = ArrayUtils.addAll(args.asInstanceOf[Array[Object]],
+        LinkisMainHelper.getExtraSpringOptions(MAIN_PROGRAM_NAME).asInstanceOf[Array[Object]]).asInstanceOf[Array[String]]
+      val argsString = StringUtils.join(allArgs.asInstanceOf[Array[Object]], '\n')
+      val startLog = String.format(s"Ready to start $serviceName with args: $argsString.")
+      LOG.info(startLog)
+      DataWorkCloudApplication.main(allArgs)
+    }
   }
+
 }
