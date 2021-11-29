5343534e5e535042a8fe017326e13c469e062b8e
hch666888 <hch6688xl@sina.com>
2021-11-29 10:15:45 +0800

    1
    ~

--- exchangis-project/exchangis-project-server/src/main/java/com/webank/wedatasphere/exchangis/project/server/dto/ExchangisProjectGetDTO.java
+++ exchangis-project/exchangis-project-server/src/main/java/com/webank/wedatasphere/exchangis/project/server/dto/ExchangisProjectGetDTO.java
@@ -1,8 +1,36 @@
 package com.webank.wedatasphere.exchangis.project.server.dto;
 
+import com.webank.wedatasphere.exchangis.project.server.entity.ExchangisProject;
+
+import java.util.Optional;
+
 public class ExchangisProjectGetDTO {
+
+    public ExchangisProjectGetDTO() {
+
+    }
+
+    public  ExchangisProjectGetDTO(ExchangisProject project) {
+        if (null == project) {
+            return;
+        }
+        this.setId(String.valueOf(project.getId()));
+        Optional.ofNullable(project.getDssProjectId()).ifPresent(dssProjectId -> {
+            this.setDssProjectId(String.valueOf(project.getDssProjectId()));
+        });
+        this.setName(project.getName());
+        this.setWorkspaceName(project.getWorkspaceName());
+        this.setDescription(project.getDescription());
+        this.setTags(project.getTags());
+        this.setEditUsers(project.getEditUsers());
+        this.setViewUsers(project.getViewUsers());
+        this.setExecUsers(project.getExecUsers());
+    }
+
     private String id;
+    private String dssProjectId;
     private String name;
+    private String workspaceName;
     private String description;
     private String tags;
     private String editUsers;
@@ -17,6 +45,10 @@ public class ExchangisProjectGetDTO {
         this.id = id;
     }
 
+    public String getDssProjectId() { return dssProjectId; }
+
+    public void setDssProjectId(String dssProjectId) { this.dssProjectId = dssProjectId; }
+
     public String getName() {
         return name;
     }
@@ -24,6 +56,10 @@ public class ExchangisProjectGetDTO {
     public void setName(String name) {
         this.name = name;
     }
+
+    public String getWorkspaceName() { return workspaceName; }
+
+    public void setWorkspaceName(String workspaceName) { this.workspaceName = workspaceName; }
 
     public String getDescription() {
         return description;
