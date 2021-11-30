c1b615841bf411b32cb1d22983259af842e8e028
Dlimeng <77954309@qq.com>
2021-11-30 18:35:38 +0800

    plugin

--- exchangis-plugins/exchangis-appconn/src/main/java/com/webank/wedatasphere/exchangis/appconn/ExchangisStructureIntegrationStandard.java
+++ exchangis-plugins/exchangis-appconn/src/main/java/com/webank/wedatasphere/exchangis/appconn/ExchangisStructureIntegrationStandard.java
@@ -2,7 +2,7 @@ package com.webank.wedatasphere.exchangis.appconn;
 
 import com.webank.wedatasphere.dss.standard.app.structure.AbstractStructureIntegrationStandard;
 import com.webank.wedatasphere.dss.standard.app.structure.project.ProjectService;
-import com.webank.wedatasphere.exchangis.appconn.project.ExchangisProjectService;
+import com.webank.wedatasphere.exchangis.appconn.service.ExchangisProjectService;
 
 public class ExchangisStructureIntegrationStandard extends AbstractStructureIntegrationStandard {
     @Override
