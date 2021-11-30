25878dccd537b4c7912d9af6d58bba45a7f90647
Dlimeng <77954309@qq.com>
2021-11-30 18:35:38 +0800

    plugin

---
+++ exchangis-plugins/exchangis-appconn/src/main/java/com/webank/wedatasphere/exchangis/appconn/service/ExchangisCRUDService.java
@@ -0,0 +1,35 @@
+package com.webank.wedatasphere.exchangis.appconn.service;
+
+import com.webank.wedatasphere.dss.standard.app.development.operation.RefCopyOperation;
+import com.webank.wedatasphere.dss.standard.app.development.operation.RefCreationOperation;
+import com.webank.wedatasphere.dss.standard.app.development.operation.RefDeletionOperation;
+import com.webank.wedatasphere.dss.standard.app.development.operation.RefUpdateOperation;
+import com.webank.wedatasphere.dss.standard.app.development.service.AbstractRefCRUDService;
+import com.webank.wedatasphere.exchangis.appconn.operation.ExchangisCreationOperation;
+import com.webank.wedatasphere.exchangis.appconn.operation.ExchangisDeletionOperation;
+import com.webank.wedatasphere.exchangis.appconn.operation.ExchangisUpdateOperation;
+
+public class ExchangisCRUDService  extends AbstractRefCRUDService {
+
+    @Override
+    protected RefCreationOperation createRefCreationOperation() {
+        return new ExchangisCreationOperation(this);
+    }
+
+
+    @Override
+    protected RefCopyOperation createRefCopyOperation() {
+        return null;
+    }
+
+    @Override
+    protected RefUpdateOperation createRefUpdateOperation() {
+        return new ExchangisUpdateOperation(this);
+    }
+
+    @Override
+    protected RefDeletionOperation createRefDeletionOperation() {
+        return new ExchangisDeletionOperation(this);
+
+    }
+}
