63389aafef651b352876339d03b5445f10f0e3f5
hch666888 <hch6688xl@sina.com>
2021-12-07 17:35:20 +0800

    1
    1

---
+++ exchangis-project/exchangis-project-server/src/main/java/com/webank/wedatasphere/exchangis/project/server/dao/ExchangisProjectRelationMapper.java
@@ -0,0 +1,9 @@
+package com.webank.wedatasphere.exchangis.project.server.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.webank.wedatasphere.exchangis.project.server.entity.ExchangisProjectRelation;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ExchangisProjectRelationMapper extends BaseMapper<ExchangisProjectRelation> {
+}
