af2318efaf0c6bbb81b996d29d497477ed9e9170
Dlimeng <77954309@qq.com>
2021-12-15 14:20:29 +0800

    plugin

--- exchangis-plugins/exchangis-appconn/src/main/java/com/webank/wedatasphere/exchangis/appconn/operation/ExchangisImportOperation.java
+++ exchangis-plugins/exchangis-appconn/src/main/java/com/webank/wedatasphere/exchangis/appconn/operation/ExchangisImportOperation.java
@@ -28,13 +28,25 @@ public class ExchangisImportOperation implements RefImportOperation<ImportReques
 
     @Override
     public ResponseRef importRef(ImportRequestRef importRequestRef) throws ExternalOperationFailedException {
-        String url = getBaseUrl() + "/import";
-        importRequestRef.getParameters().forEach((k,v)->{
-            logger.info("importRef job=>k:{},v:{}",k,v.toString());
-        });
+        String url = getBaseUrl() + "/relation";
+        logger.info("importRef job=>parameter:{} ||name:{}",importRequestRef.getParameters().toString(),importRequestRef.getName());
 
+        /**
+         *   private String id;
+         *     @NotNull
+         *     @NotEmpty
+         *     private String projectId;
+         *     @NotNull
+         *     @NotEmpty
+         *     private String nodeId;
+         *     private String projectVersion;
+         *     private String flowVersion;
+         *     private String resourceId;
+         *     private String version;
+         */
         ExchangisPostAction exchangisPostAction = new ExchangisPostAction();
         exchangisPostAction.setUser(importRequestRef.getParameter("user").toString());
+
         exchangisPostAction.addRequestPayload("projectId", importRequestRef.getParameter("projectId"));
         exchangisPostAction.addRequestPayload("projectVersion", "v1");
         exchangisPostAction.addRequestPayload("flowVersion", importRequestRef.getParameter("orcVersion"));
@@ -49,9 +61,9 @@ public class ExchangisImportOperation implements RefImportOperation<ImportReques
             exchangisPostAction.setUrl(ssoUrlBuilderOperation.getBuiltUrl());
             HttpResult httpResult = (HttpResult) this.ssoRequestOperation.requestWithSSO(ssoUrlBuilderOperation, exchangisPostAction);
             responseRef = new ExchangisCommonResponseRef(httpResult.getResponseBody());
-
+            logger.info("import job body:{}",responseRef.getResponseBody());
         } catch (Exception e){
-            throw new ExternalOperationFailedException(31022, "Export Visualis Exception", e);
+            throw new ExternalOperationFailedException(31025, "import exchangis exception", e);
         }
         return responseRef;
     }
--- exchangis-plugins/exchangis-appconn/src/main/java/com/webank/wedatasphere/exchangis/appconn/operation/ExchangisQueryOperation.java
+++ exchangis-plugins/exchangis-appconn/src/main/java/com/webank/wedatasphere/exchangis/appconn/operation/ExchangisQueryOperation.java
@@ -4,19 +4,12 @@ import com.webank.wedatasphere.dss.common.utils.DSSCommonUtils;
 import com.webank.wedatasphere.dss.standard.app.development.operation.RefQueryOperation;
 import com.webank.wedatasphere.dss.standard.app.development.ref.OpenRequestRef;
 import com.webank.wedatasphere.dss.standard.app.development.service.DevelopmentService;
-import com.webank.wedatasphere.dss.standard.app.sso.builder.SSOUrlBuilderOperation;
 import com.webank.wedatasphere.dss.standard.app.sso.request.SSORequestOperation;
 import com.webank.wedatasphere.dss.standard.common.entity.ref.ResponseRef;
 import com.webank.wedatasphere.dss.standard.common.exception.operation.ExternalOperationFailedException;
 import com.webank.wedatasphere.exchangis.appconn.config.ExchangisConfig;
-import com.webank.wedatasphere.exchangis.appconn.model.ExchangisGetAction;
-import com.webank.wedatasphere.exchangis.appconn.ref.ExchangisCommonResponseRef;
 import com.webank.wedatasphere.exchangis.appconn.ref.ExchangisOpenRequestRef;
 import com.webank.wedatasphere.exchangis.appconn.ref.ExchangisOpenResponseRef;
-import com.webank.wedatasphere.exchangis.appconn.utils.AppconnUtils;
-import com.webank.wedatasphere.linkis.httpclient.request.HttpAction;
-import com.webank.wedatasphere.linkis.httpclient.response.HttpResult;
-import com.webank.wedatasphere.linkis.server.BDPJettyServerHelper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,7 +18,6 @@ import java.util.Map;
 
 public class ExchangisQueryOperation implements RefQueryOperation<OpenRequestRef> {
     private final static Logger logger = LoggerFactory.getLogger(ExchangisQueryOperation.class);
-
 
     private DevelopmentService developmentService;
     private SSORequestOperation ssoRequestOperation;
@@ -37,43 +29,9 @@ public class ExchangisQueryOperation implements RefQueryOperation<OpenRequestRef
     @Override
     public ResponseRef query(OpenRequestRef openRequestRef) throws ExternalOperationFailedException {
         ExchangisOpenRequestRef exchangisOpenRequestRef = (ExchangisOpenRequestRef) openRequestRef;
-
         logger.info("query job=>jobContent:{} ||,projectId:{}  ||,projectName:{}  ||,parameters:{} ||,type:{}",exchangisOpenRequestRef.getJobContent(),exchangisOpenRequestRef.getProjectId(),exchangisOpenRequestRef.getProjectName(),exchangisOpenRequestRef.getParameters().toString(),exchangisOpenRequestRef.getType());
         try {
-            ExchangisGetAction exchangisGetAction = new ExchangisGetAction();
-
-            String name = exchangisOpenRequestRef.getName();
-            String type = openRequestRef.getType();
-            String jobType = ExchangisConfig.JOB_TYPE_OFFLINE.toString();
-            Map<String, Object> contextVal = null;
-            try {
-//                String contextID = exchangisOpenRequestRef.getJobContent().get("contextID").toString();
-//                Map contextIDMap =  BDPJettyServerHelper.jacksonJson().readValue(contextID, Map.class);
-//                contextVal = (Map<String, Object>) contextIDMap.get("value");
-//                logger.info("contextVal {}",contextVal.toString());
-            }catch (Exception e){
-                throw new ExternalOperationFailedException(31023, "Get node Id failed!", e);
-            }
-
-           // logger.info("exchangisCommonResponseRef nodeId {}",nodeId);
-            String jobUrl=getBaseUrl()+"/dss/";
-
-            SSOUrlBuilderOperation ssoUrlBuilderOperation = exchangisOpenRequestRef.getWorkspace().getSSOUrlBuilderOperation().copy();
-            ssoUrlBuilderOperation.setAppName(ExchangisConfig.EXCHANGIS_APPCONN_NAME);
-            ssoUrlBuilderOperation.setReqUrl(jobUrl);
-            ssoUrlBuilderOperation.setWorkspace(exchangisOpenRequestRef.getWorkspace().getWorkspaceName());
-            HttpResult httpResult = (HttpResult) this.ssoRequestOperation.requestWithSSO(ssoUrlBuilderOperation, exchangisGetAction);
-            ExchangisCommonResponseRef exchangisCommonResponseRef = new ExchangisCommonResponseRef(httpResult.getResponseBody());
-            logger.info("exchangisCommonResponseRef map {}",exchangisCommonResponseRef.getResponseBody());
-            if(exchangisCommonResponseRef.isFailed()){
-                logger.error(exchangisCommonResponseRef.getResponseBody());
-                throw new ExternalOperationFailedException(31022,exchangisCommonResponseRef.getErrorMsg());
-            }
-
-
-            String jobId = exchangisCommonResponseRef.toMap().get("id").toString();
-
-
+            String jobId = ((Map<String,Object>)((Map<String,Object>)exchangisOpenRequestRef.getJobContent().get("data")).get("result")).get("id").toString();
             String baseUrl = exchangisOpenRequestRef.getParameter("redirectUrl").toString();
             String jumpUrl = baseUrl;
             logger.info("exchangisCommonResponseRef jobId {},baseUrl {}",jobId,baseUrl);
@@ -90,10 +48,6 @@ public class ExchangisQueryOperation implements RefQueryOperation<OpenRequestRef
         } catch (Exception e) {
             throw new ExternalOperationFailedException(31022, "Failed to parse jobContent ", e);
         }
-    }
-
-    private String getBaseUrl(){
-        return developmentService.getAppInstance().getBaseUrl() + ExchangisConfig.BASEURL;
     }
 
     @Override
