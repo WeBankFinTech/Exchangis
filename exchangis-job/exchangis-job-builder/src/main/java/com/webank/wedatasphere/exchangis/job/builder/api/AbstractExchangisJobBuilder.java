dbaf12525462a08163abdd4bc24d1f809b18c7f3
Davidhua1996 <david_hua1996@hotmail.com>
2022-01-21 20:44:13 +0800

    Reconstruct the SqoopExchangisEngineJobBuilder

--- exchangis-job/exchangis-job-builder/src/main/java/com/webank/wedatasphere/exchangis/job/builder/api/AbstractExchangisJobBuilder.java
+++ exchangis-job/exchangis-job-builder/src/main/java/com/webank/wedatasphere/exchangis/job/builder/api/AbstractExchangisJobBuilder.java
@@ -1,7 +1,11 @@
 package com.webank.wedatasphere.exchangis.job.builder.api;
 
+import com.webank.wedatasphere.exchangis.job.builder.ExchangisJobBuilderContext;
 import com.webank.wedatasphere.exchangis.job.domain.ExchangisBase;
 import com.webank.wedatasphere.exchangis.job.domain.ExchangisJob;
+import com.webank.wedatasphere.exchangis.job.domain.params.JobParamDefine;
+import com.webank.wedatasphere.exchangis.job.domain.params.JobParamSet;
+import com.webank.wedatasphere.exchangis.job.exception.ExchangisJobException;
 import com.webank.wedatasphere.exchangis.job.utils.TypeGenericUtils;
 
 import java.lang.reflect.ParameterizedType;
@@ -32,4 +36,16 @@ public abstract class AbstractExchangisJobBuilder<T extends ExchangisJob, E exte
         return true;
     }
 
+
+    @Override
+    public E build(T inputJob, E expectOut, ExchangisJobBuilderContext ctx) throws ExchangisJobException {
+        JobParamDefine.defaultParam.set(new JobParamSet());
+        try {
+            return buildJob(inputJob, expectOut, ctx);
+        } finally{
+            JobParamDefine.defaultParam.remove();
+        }
+    }
+
+    public abstract E buildJob(T inputJob, E expectOut, ExchangisJobBuilderContext ctx) throws ExchangisJobException;
 }
