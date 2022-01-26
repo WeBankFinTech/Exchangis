9f96db0cd5fca9ba10f1da9ee0a8845376871c80
Davidhua1996 <david_hua1996@hotmail.com>
2022-01-26 16:47:57 +0800

    Execution Debug Fixed in concurrency

--- exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/dao/LaunchedJobDao.java
+++ exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/dao/LaunchedJobDao.java
@@ -10,25 +10,30 @@ import java.util.Date;
  */
 public interface LaunchedJobDao {
     /**
-     * insert launchJob
+     * insert launchedJob
      * @param launchedExchangisJobEntity entity
      */
 
     void insertLaunchedJob(LaunchedExchangisJobEntity launchedExchangisJobEntity);
 
     /**
-     * delete launchJob
+     * delete launchedJob
      * @param jobExecutionId execution id
      */
 
     void deleteLaunchedJob(@Param("jobExecutionId")String jobExecutionId);
 
     /**
-     * upgrade launchJob
+     * upgrade launchedJob
      * @param launchedExchangisJobEntity entity
      */
     void upgradeLaunchedJob(LaunchedExchangisJobEntity launchedExchangisJobEntity);
 
+    /**
+     * Update launch info
+     * @param launchedExchangisJobEntity entity
+     */
+    void updateLaunchInfo(LaunchedExchangisJobEntity launchedExchangisJobEntity);
     /**
      * search launchJob
      * @param jobExecutionId execution id
@@ -37,10 +42,30 @@ public interface LaunchedJobDao {
     LaunchedExchangisJobEntity searchLaunchedJob(@Param("jobExecutionId")String jobExecutionId);
 
     /**
-     * upgrade launchJob status
+     * upgrade launchedJob status
      * @param jobExecutionId execution id
      * @param status status
      */
 
     void upgradeLaunchedJobStatus(@Param("jobExecutionId")String jobExecutionId, @Param("status") String status, @Param("updateTime")Date updateTime);
+
+    /**
+     * Try to upgrade launchedJob status in version control
+     * @param jobExecutionId execution id
+     * @param status update status
+     * @param launchableTaskNum expected launchable task number
+     * @param updateTime updateTime
+     */
+    int upgradeLaunchedJobStatusInVersion(@Param("jobExecutionId")String jobExecutionId, @Param("status") String status,
+                                       @Param("launchableTaskNum") Integer launchableTaskNum,
+                                       @Param("updateTime")Date updateTime);
+
+    /**
+     * Try to upgrade launchedJob progress in version control
+     * @param jobExecutionId execution id
+     * @param totalTaskProgress progress of total related task
+     * @param updateTime updateTime
+     */
+    int upgradeLaunchedJobProgressInVersion(@Param("jobExecutionId")String jobExecutionId, @Param("totalTaskProgress") Float totalTaskProgress,
+                                         @Param("updateTime")Date updateTime);
 }
