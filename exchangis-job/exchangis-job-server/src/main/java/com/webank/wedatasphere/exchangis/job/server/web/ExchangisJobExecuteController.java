30bdacff6c100f1b9ac5764597e8e90dcc7bd31e
Davidhua1996 <david_hua1996@hotmail.com>
2022-02-06 18:02:38 +0800

    Complete the strategy of fetching task logs

--- exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/web/ExchangisJobExecuteController.java
+++ exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/web/ExchangisJobExecuteController.java
@@ -2,6 +2,7 @@ package com.webank.wedatasphere.exchangis.job.server.web;
 
 import com.webank.wedatasphere.exchangis.datasource.core.utils.Json;
 import com.webank.wedatasphere.exchangis.job.domain.ExchangisJobInfo;
+import com.webank.wedatasphere.exchangis.job.log.LogQuery;
 import com.webank.wedatasphere.exchangis.job.server.exception.ExchangisJobServerException;
 import com.webank.wedatasphere.exchangis.job.server.service.ExchangisJobService;
 import com.webank.wedatasphere.exchangis.job.server.service.impl.DefaultJobExecuteService;
@@ -113,15 +114,17 @@ public class ExchangisJobExecuteController {
                                         @RequestParam(value = "pageSize", required = false) Integer pageSize,
                                         @RequestParam(value = "ignoreKeywords", required = false) String ignoreKeywords,
                                         @RequestParam(value = "onlyKeywords", required = false) String onlyKeywords,
-                                        @RequestParam(value = "lastRows", required = false) Integer lastRows) {
+                                        @RequestParam(value = "lastRows", required = false) Integer lastRows, HttpServletRequest request) {
 
         Message result = Message.ok("Submitted succeed(提交成功)！");
+        LogQuery logQuery = new LogQuery(fromLine, pageSize,
+                ignoreKeywords, onlyKeywords, lastRows);
         try {
             ExchangisCategoryLogVo categoryLogVo = this.executeService
-                    .getJobLogInfo(jobExecutionId, fromLine, pageSize, ignoreKeywords, onlyKeywords, lastRows);
+                    .getJobLogInfo(jobExecutionId, logQuery, SecurityFilter.getLoginUsername(request));
             result.setData(Json.convert(categoryLogVo, Map.class, String.class, Object.class));
         } catch (ExchangisJobServerException e) {
-            String message = "Error occur while querying job log: [id: " + jobExecutionId  +"]";
+            String message = "Error occur while querying job log: [job_execution_id: " + jobExecutionId  +"]";
             LOG.error(message, e);
             result = Message.error(message + ", reason: " + e.getMessage());
         }
