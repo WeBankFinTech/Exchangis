646c0152310012a971c2d34cc922cbdd1b9d653b
Magnus Wang <mingfengx.wang@intel.com>
2022-02-21 15:59:51 +0800

    隐藏搜索库表 & 独立单页

--- web/src/pages/jobManagementItem/oldComponents/configModal.vue
+++ web/src/pages/jobManagementItem/oldComponents/configModal.vue
@@ -11,16 +11,13 @@
       :rules="rules"
       :model="formState"
       :label-col="{ span: 4 }"
-      class="config-modal-form"
     >
-      <div class="cm-title">
-        <span>任务配置</span>
-      </div>
+      <h2>任务配置</h2>
       <a-form-item label="执行用户" name="proxyUser">
-        <a-input v-model:value="formState.proxyUser" :style="{ width: '366px'}"/>
+        <a-input v-model:value="formState.proxyUser" />
       </a-form-item>
-      <a-form-item label="执行节点" name="executeNode" >
-        <a-input v-model:value="formState.executeNode" :style="{ width: '366px'}" />
+      <a-form-item label="执行节点" name="executeNode">
+        <a-input v-model:value="formState.executeNode" />
       </a-form-item>
       <a-form-item label="同步方式" name="syncType">
         <a-radio-group v-model:value="formState.syncType" name="syncType">
@@ -28,37 +25,28 @@
           <a-radio :value="'INCREMENTAL'"> 增量 </a-radio>
         </a-radio-group>
       </a-form-item>
-      <div class="cm-title">
-        <span>任务变量</span>
-      </div>
-      <div class="cm-button" @click="createTask" style="cursor: pointer;">
-        <PlusOutlined style="margin-right: 8px;font-size: 12px;"/>
-        <span>添加变量</span>
-      </div>
-      <div style="overflow-y: auto;max-height: 150px" id="variable-modification">
-        <div
-          v-for="(item, index) in formState.jobParams"
-          style="overflow: hidden;position: relative"
-          :key="index"
-        >
-          <a-form-item class="w50 fl" :label="index + 1" name="jobParamsKey" labelAlign='left' :label-col='{ span: 3 }'>
-            <a-input v-model:value="item.key" :style="{ width: '194px'}"/>
-          </a-form-item>
-          <span class="fl separator">=</span>
-          <a-form-item class="w40 fl" name="jobParamsValue">
-            <a-input v-model:value="item.value" :style="{ width: '194px'}"/>
-          </a-form-item>
-          <DeleteOutlined class="delete-icon" @click="deleteTask(index)"/>
-        </div>
+      <h2>任务变量 <PlusOutlined class="fr" @click="createTask" /></h2>
+      <div
+        v-for="(item, index) in formState.jobParams"
+        style="overflow: hidden"
+        :key="index"
+      >
+        <a-form-item class="w40 fl" :label="index + 1" name="jobParamsKey">
+          <a-input v-model:value="item.key" />
+        </a-form-item>
+        <span class="fl separator">=</span>
+        <a-form-item class="w40 fl" name="jobParamsValue">
+          <a-input v-model:value="item.value" />
+        </a-form-item>
       </div>
     </a-form>
   </a-modal>
 </template>
 
 <script>
-import { toRaw, ref, watch, reactive, watchEffect, nextTick } from "vue";
+import { toRaw, ref, watch, reactive, watchEffect } from "vue";
 import { message } from "ant-design-vue";
-import { PlusOutlined , DeleteOutlined} from "@ant-design/icons-vue";
+import { PlusOutlined } from "@ant-design/icons-vue";
 import { createProject, getProjectById, updateProject } from "@/common/service";
 import { cloneDeep } from "lodash-es";
 
@@ -88,7 +76,6 @@ export default {
   name: "JobManagementConfigModal",
   components: {
     PlusOutlined,
-    DeleteOutlined
   },
   props: {
     // 是否可见
@@ -143,33 +130,13 @@ export default {
       }
     );
 
-    async function createTask() {
-      if (formState.jobParams.length) {
-        let cur = formState.jobParams[formState.jobParams.length - 1]
-        if (!cur.key || !cur.value) {
-          return message.error('请先填写变量')
-        }
-      }
+    const createTask = () => {
       formState.jobParams.push({ key: "", value: "" });
-      await nextTick()
-      const varMo = document.querySelector('#variable-modification');
-      varMo.scrollTop = varMo.scrollHeight;
-    }
-
-    const deleteTask = (index) => {
-      formState.jobParams.splice(index, 1)
-    }
+    };
 
     const handleOk = async () => {
       await formRef.value.validate();
       const formatData = cloneDeep(formState);
-      if (formatData.jobParams) {
-        for (let i = 0; i < formatData.jobParams.length; i++) {
-          if (!formatData.jobParams[i].key || !formatData.jobParams[i].value) {
-            return message.error("任务变量不能为空，请填写或删除");
-          }
-        }
-      }
       // try {
       //   if (this.mode === "create") {
       //     confirmLoading.value = true;
@@ -194,7 +161,6 @@ export default {
       formState,
       createTask,
       handleOk,
-      deleteTask
     };
   },
 };
@@ -210,46 +176,7 @@ export default {
 .w40 {
   width: 40%;
 }
-.w50 {
-  width: 50%;
-}
 .separator {
   margin: 5px;
-}
-
-.config-modal-form {
-  margin-left: 10px;
-}
-
-.cm-title {
-  font-family: PingFangSC-Medium;
-  font-size: 14px;
-  color: rgba(0, 0, 0, 0.85);
-  line-height: 22px;
-  font-weight: 500;
-  height: 22px;
-  margin-bottom: 16px;
-  text-align: left;
-}
-
-.cm-button {
-  height: 32px;
-  width: 444px;
-  line-height: 32px;
-  background: #f8f9fc;
-  border: 1px dashed #dee4ec;
-  border-radius: 4px;
-  font-family: PingFangSC-Regular;
-  font-size: 14px;
-  color: rgba(0, 0, 0, 0.65);
-  text-align: center;
-  font-weight: 400;
-  margin-bottom: 16px;
-}
-.delete-icon {
-  cursor: pointer;
-  right: 0;
-  top: 8px;
-  position: absolute;
 }
 </style>
