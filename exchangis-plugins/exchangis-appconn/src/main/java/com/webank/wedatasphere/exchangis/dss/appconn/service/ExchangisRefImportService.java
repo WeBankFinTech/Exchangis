000f986aaf42183adcc898e1c97c31ec281df0d5
wushengyeyouya <690574002@qq.com>
2022-05-10 23:03:36 +0800

    Add ExchangisAppConn to adapt the DSS1.1.0 AppConn architecture.

--- exchangis-plugins/exchangis-appconn/src/main/java/com/webank/wedatasphere/exchangis/dss/appconn/service/ExchangisRefImportService.java
+++ exchangis-plugins/exchangis-appconn/src/main/java/com/webank/wedatasphere/exchangis/dss/appconn/service/ExchangisRefImportService.java
@@ -1,19 +1,17 @@
 package com.webank.wedatasphere.exchangis.dss.appconn.service;
 
 
-import com.webank.wedatasphere.dss.standard.app.development.operation.RefImportOperation;
 import com.webank.wedatasphere.dss.standard.app.development.service.AbstractRefImportService;
-import com.webank.wedatasphere.dss.standard.common.entity.ref.RequestRef;
-import com.webank.wedatasphere.exchangis.dss.appconn.operation.ref.ExchangisImportOperation;
+import com.webank.wedatasphere.exchangis.dss.appconn.operation.development.ExchangisImportOperation;
 
 /**
  * Ref import service
  */
 public class ExchangisRefImportService extends AbstractRefImportService {
+
     @Override
-    @SuppressWarnings("unchecked")
-    protected <K extends RequestRef> RefImportOperation<K> createRefImportOperation() {
-        return (RefImportOperation<K>) new ExchangisImportOperation(this);
+    protected ExchangisImportOperation createRefImportOperation() {
+        return new ExchangisImportOperation();
     }
 
 }
