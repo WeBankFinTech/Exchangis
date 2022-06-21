c7277ecdfb2870062326aeb1ca45f3a257d5f0fa
jefftlin <jefft_lin@163.com>
2022-06-21 16:26:54 +0800

    Update exchangis-appconn module

--- exchangis-plugins/exchangis-appconn/src/main/java/com/webank/wedatasphere/exchangis/dss/appconn/ExchangisStructureIntegrationStandard.java
+++ exchangis-plugins/exchangis-appconn/src/main/java/com/webank/wedatasphere/exchangis/dss/appconn/ExchangisStructureIntegrationStandard.java
@@ -9,21 +9,9 @@ import com.webank.wedatasphere.exchangis.dss.appconn.service.ExchangisProjectSer
  */
 public class ExchangisStructureIntegrationStandard extends AbstractStructureIntegrationStandard {
 
-    private static ExchangisStructureIntegrationStandard instance;
-
-    static{
-        instance = new ExchangisStructureIntegrationStandard();
-    }
-
-    /**
-     * Get the singleton instance
-     * @return structure integration
-     */
-    public static ExchangisStructureIntegrationStandard getInstance(){
-        return instance;
-    }
     @Override
     protected ProjectService createProjectService() {
         return new ExchangisProjectService();
     }
+
 }
