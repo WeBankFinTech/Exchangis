25da2a49762a40c6c9aec1ddbeb39920c7ce33ea
jefftlin <jefft_lin@163.com>
2022-06-21 16:26:54 +0800

    Update exchangis-appconn module

--- exchangis-plugins/exchangis-appconn/src/main/java/com/webank/wedatasphere/exchangis/dss/appconn/operation/project/ExchangisProjectDeletionOperation.java
+++ exchangis-plugins/exchangis-appconn/src/main/java/com/webank/wedatasphere/exchangis/dss/appconn/operation/project/ExchangisProjectDeletionOperation.java
@@ -1,73 +1,35 @@
 package com.webank.wedatasphere.exchangis.dss.appconn.operation.project;
 
-import com.webank.wedatasphere.dss.standard.app.sso.request.SSORequestOperation;
-import com.webank.wedatasphere.dss.standard.app.structure.StructureService;
+import com.webank.wedatasphere.dss.standard.app.sso.origin.request.action.DSSPostAction;
+import com.webank.wedatasphere.dss.standard.app.structure.AbstractStructureOperation;
 import com.webank.wedatasphere.dss.standard.app.structure.project.ProjectDeletionOperation;
-import com.webank.wedatasphere.dss.standard.app.structure.project.ProjectRequestRef;
-import com.webank.wedatasphere.dss.standard.app.structure.project.ProjectResponseRef;
+import com.webank.wedatasphere.dss.standard.app.structure.project.ref.RefProjectContentRequestRef;
+import com.webank.wedatasphere.dss.standard.common.entity.ref.ResponseRef;
 import com.webank.wedatasphere.dss.standard.common.exception.operation.ExternalOperationFailedException;
 import com.webank.wedatasphere.exchangis.dss.appconn.constraints.Constraints;
-import com.webank.wedatasphere.exchangis.dss.appconn.request.action.ExchangisDeleteAction;
-import com.webank.wedatasphere.exchangis.dss.appconn.ref.ExchangisProjectResponseRef;
-import com.webank.wedatasphere.exchangis.dss.appconn.request.action.ExchangisEntityPostAction;
-import com.webank.wedatasphere.exchangis.dss.appconn.response.result.ExchangisEntityRespResult;
-import org.apache.linkis.httpclient.request.HttpAction;
-import org.apache.linkis.httpclient.response.HttpResult;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.webank.wedatasphere.exchangis.dss.appconn.utils.ExchangisHttpUtils;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import static com.webank.wedatasphere.exchangis.dss.appconn.constraints.Constraints.API_REQUEST_PREFIX;
 
 
 /**
  * Project delete operation
  */
-public class ExchangisProjectDeletionOperation extends AbstractExchangisProjectOperation implements ProjectDeletionOperation {
-    private static Logger LOG = LoggerFactory.getLogger(ExchangisProjectDeletionOperation.class);
+public class ExchangisProjectDeletionOperation extends AbstractStructureOperation<RefProjectContentRequestRef.RefProjectContentRequestRefImpl, ResponseRef>
+        implements ProjectDeletionOperation<RefProjectContentRequestRef.RefProjectContentRequestRefImpl> {
 
-    private SSORequestOperation<HttpAction, HttpResult> ssoRequestOperation;
-    private StructureService structureService;
-
-    public ExchangisProjectDeletionOperation(StructureService structureService) {
-        setStructureService(structureService);
+    @Override
+    public ResponseRef deleteProject(RefProjectContentRequestRef.RefProjectContentRequestRefImpl projectRequestRef) throws ExternalOperationFailedException {
+        String url = mergeBaseUrl(mergeUrl(API_REQUEST_PREFIX,"appProject/" + projectRequestRef.getProjectName()));
+        logger.info("User {} try to delete Exchangis project with refProjectId: {}, name: {}, the url is {}.", projectRequestRef.getUserName(),
+                projectRequestRef.getRefProjectId(), projectRequestRef.getProjectName(), url);
+        DSSPostAction postAction = new DSSPostAction();
+        postAction.setUser(projectRequestRef.getUserName());
+        return ExchangisHttpUtils.getResponseRef(projectRequestRef, url, postAction, ssoRequestOperation);
     }
 
     @Override
-    protected Logger getLogger() {
-        return LOG;
-    }
-
-    @Override
-    public ProjectResponseRef deleteProject(ProjectRequestRef projectRequestRef) throws ExternalOperationFailedException {
-        LOG.info("delete project request => dss_projectId:{}, name:{}, createName:{}", projectRequestRef.getId(),
-                projectRequestRef.getName(), projectRequestRef.getCreateBy());
-        String url = requestURL("appProject/" + projectRequestRef.getName()+"/");
-        ExchangisEntityRespResult.BasicMessageEntity<Map<String, Object>> entity = requestToGetEntity(url,projectRequestRef.getWorkspace(), projectRequestRef,
-                (requestRef) ->{
-                    ExchangisEntityPostAction exchangisEntityPostAction = new ExchangisEntityPostAction();
-                    exchangisEntityPostAction.setUser(requestRef.getCreateBy());
-                    return exchangisEntityPostAction;
-                }, Map.class);
-        if (Objects.isNull(entity)){
-            throw new ExternalOperationFailedException(31020, "The response entity cannot be empty", null);
-        }
-        ExchangisEntityRespResult httpResult = entity.getResult();
-        LOG.info("delete project response => status {}, response {}", httpResult.getStatusCode(), httpResult.getResponseBody());
-        ExchangisProjectResponseRef responseRef = new ExchangisProjectResponseRef(httpResult, null);
-        responseRef.setAppInstance(structureService.getAppInstance());
-        return responseRef;
-    }
-
-    @Override
-    public void init() {
-
-    }
-
-    @Override
-    public void setStructureService(StructureService structureService) {
-        this.structureService = structureService;
-        setSSORequestService(this.structureService);
+    protected String getAppConnName() {
+        return Constraints.EXCHANGIS_APPCONN_NAME;
     }
 }
