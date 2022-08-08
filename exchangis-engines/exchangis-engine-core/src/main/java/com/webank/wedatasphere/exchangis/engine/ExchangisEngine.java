14a35a4f56318535fbe71bd81478e8dd6f69f75e
davidhua <david_hua1996@hotmail.com>
2022-08-08 20:16:39 +0800

    Complete 90 percents of exchangis-engine's core module.

--- exchangis-engines/exchangis-engine-core/src/main/java/com/webank/wedatasphere/exchangis/engine/ExchangisEngine.java
+++ exchangis-engines/exchangis-engine-core/src/main/java/com/webank/wedatasphere/exchangis/engine/ExchangisEngine.java
@@ -1,5 +1,28 @@
 package com.webank.wedatasphere.exchangis.engine;
 
+import com.webank.wedatasphere.exchangis.engine.domain.EngineSettings;
+import com.webank.wedatasphere.exchangis.engine.resource.EngineResourceContainer;
+
+/**
+ * Exchangis engine
+ */
 public interface ExchangisEngine {
-    String getEngineType();
+
+    /**
+     * engine name
+     * @return
+     */
+    String getName();
+
+    /**
+     * Settings
+     * @return settings
+     */
+    EngineSettings getSettings();
+
+    /**
+     * Resource container
+     * @return container
+     */
+    EngineResourceContainer<?, ?> getResourceContainer();
 }
---
+++ exchangis-engines/exchangis-engine-core/src/main/java/com/webank/wedatasphere/exchangis/engine/resource/bml/BmlEngineResourceUploader.java
@@ -0,0 +1,4 @@
+package com.webank.wedatasphere.exchangis.engine.resource.bml;
+
+public class BmlEngineResourceUploader {
+}
