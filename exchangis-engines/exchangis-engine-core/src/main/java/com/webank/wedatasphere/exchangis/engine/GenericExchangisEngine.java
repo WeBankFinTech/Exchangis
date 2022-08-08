aba424950708896f241bb5e29a4a5ffc5fa7e6f6
davidhua <david_hua1996@hotmail.com>
2022-08-08 20:16:39 +0800

    Complete 90 percents of exchangis-engine's core module.

--- exchangis-engines/exchangis-engine-core/src/main/java/com/webank/wedatasphere/exchangis/engine/GenericExchangisEngine.java
+++ exchangis-engines/exchangis-engine-core/src/main/java/com/webank/wedatasphere/exchangis/engine/GenericExchangisEngine.java
@@ -1,4 +1,44 @@
 package com.webank.wedatasphere.exchangis.engine;
 
-public class GenericExchangisEngine {
+import com.webank.wedatasphere.exchangis.engine.domain.EngineSettings;
+import com.webank.wedatasphere.exchangis.engine.resource.EngineResourceContainer;
+
+import java.util.Objects;
+
+/**
+ * Generic implement
+ */
+public class GenericExchangisEngine implements ExchangisEngine{
+
+    @Override
+    public String getName() {
+        return Objects.nonNull(settings) ? settings.getName(): null;
+    }
+
+    /**
+     * Settings
+     */
+    private EngineSettings settings;
+
+    /**
+     * Resource container
+     */
+    private EngineResourceContainer<?, ?> resourceContainer;
+
+    public EngineSettings getSettings() {
+        return settings;
+    }
+
+    public void setSettings(EngineSettings settings) {
+        this.settings = settings;
+    }
+
+    public EngineResourceContainer<?, ?> getResourceContainer() {
+        return resourceContainer;
+    }
+
+    public void setResourceContainer(EngineResourceContainer<?, ?> resourceContainer) {
+        this.resourceContainer = resourceContainer;
+    }
+
 }
