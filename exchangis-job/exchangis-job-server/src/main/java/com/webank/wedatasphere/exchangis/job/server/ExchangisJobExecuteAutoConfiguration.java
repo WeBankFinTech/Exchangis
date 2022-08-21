e33181fd4e452366ed86e467057fe68c18ac7d80
davidhua <david_hua1996@hotmail.com>
2022-08-21 22:41:15 +0800

    Add Elastic search params mapping.

--- exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/ExchangisJobExecuteAutoConfiguration.java
+++ exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/ExchangisJobExecuteAutoConfiguration.java
@@ -29,6 +29,7 @@ import org.apache.linkis.scheduler.queue.ConsumerManager;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 
 import java.util.List;
 import java.util.Objects;
@@ -38,6 +39,7 @@ import java.util.Optional;
  * Auto configure the beans in job execution
  */
 @Configuration
+@DependsOn("springContextHolder")
 public class ExchangisJobExecuteAutoConfiguration {
 
     @Bean
