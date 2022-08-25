801866f8250aa599d35424dea80e151c907ddec1
davidhua <david_hua1996@hotmail.com>
2022-08-25 17:31:47 +0800

    Auto to fetch status of job when subscribing the task.

--- exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/service/impl/DefaultTaskObserverService.java
+++ exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/service/impl/DefaultTaskObserverService.java
@@ -1,8 +1,11 @@
 package com.webank.wedatasphere.exchangis.job.server.service.impl;
 
 import com.webank.wedatasphere.exchangis.job.launcher.domain.LaunchableExchangisTask;
+import com.webank.wedatasphere.exchangis.job.launcher.domain.task.TaskStatus;
+import com.webank.wedatasphere.exchangis.job.launcher.entity.LaunchedExchangisJobEntity;
 import com.webank.wedatasphere.exchangis.job.launcher.entity.LaunchedExchangisTaskEntity;
 import com.webank.wedatasphere.exchangis.job.server.mapper.LaunchableTaskDao;
+import com.webank.wedatasphere.exchangis.job.server.mapper.LaunchedJobDao;
 import com.webank.wedatasphere.exchangis.job.server.mapper.LaunchedTaskDao;
 import com.webank.wedatasphere.exchangis.job.server.service.TaskObserverService;
 import org.springframework.stereotype.Service;
@@ -10,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * Task observer service
@@ -26,6 +30,8 @@ public class DefaultTaskObserverService implements TaskObserverService {
     @Resource
     private LaunchedTaskDao launchedTaskDao;
 
+    @Resource
+    private LaunchedJobDao launchedJobDao;
     @Override
     public List<LaunchableExchangisTask> onPublishLaunchableTask(int limitSize) {
         return launchableTaskDao.getTaskToLaunch(limitSize);
@@ -35,7 +41,13 @@ public class DefaultTaskObserverService implements TaskObserverService {
     @Transactional(rollbackFor = Exception.class)
     public boolean subscribe(LaunchableExchangisTask task) {
         LaunchedExchangisTaskEntity taskEntity = new LaunchedExchangisTaskEntity(task);
-        int result = this.launchedTaskDao.insertLaunchedTaskOrUpdate(taskEntity);
-        return result == 1;
+        LaunchedExchangisJobEntity jobEntity = launchedJobDao.searchLaunchedJob(task.getJobExecutionId());
+        if (Objects.isNull(jobEntity) || TaskStatus.isCompleted(jobEntity.getStatus())){
+            taskEntity.setStatus(jobEntity.getStatus());
+            this.launchedTaskDao.insertLaunchedTaskOrUpdate(taskEntity);
+            return false;
+        } else {
+            return this.launchedTaskDao.insertLaunchedTaskOrUpdate(taskEntity) == 1;
+        }
     }
 }
