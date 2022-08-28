46ce8b6046f3b9be78b5fa137e297423567f8a5e
davidhua <david_hua1996@hotmail.com>
2022-08-28 18:50:28 +0800

    1) Enlarge the max connection number of linkis client. 2) Fix the problem in FlexibleTenancyLoadBalancer.

--- exchangis-job/exchangis-job-launcher/src/main/java/com/webank/wedatasphere/exchangis/job/launcher/linkis/LinkisExchangisTaskLauncher.java
+++ exchangis-job/exchangis-job-launcher/src/main/java/com/webank/wedatasphere/exchangis/job/launcher/linkis/LinkisExchangisTaskLauncher.java
@@ -1,20 +1,29 @@
 package com.webank.wedatasphere.exchangis.job.launcher.linkis;
 
 import com.webank.wedatasphere.exchangis.common.linkis.ClientConfiguration;
-import com.webank.wedatasphere.exchangis.job.domain.ExchangisTask;
 import com.webank.wedatasphere.exchangis.job.enums.EngineTypeEnum;
 import com.webank.wedatasphere.exchangis.job.launcher.AccessibleLauncherTask;
 import com.webank.wedatasphere.exchangis.job.launcher.exception.ExchangisTaskLaunchException;
-import com.webank.wedatasphere.exchangis.job.launcher.ExchangisLauncherConfiguration;
 import com.webank.wedatasphere.exchangis.job.launcher.ExchangisTaskLaunchManager;
 import com.webank.wedatasphere.exchangis.job.launcher.ExchangisTaskLauncher;
 import com.webank.wedatasphere.exchangis.job.launcher.domain.LaunchableExchangisTask;
 import com.webank.wedatasphere.exchangis.job.launcher.domain.LaunchedExchangisTask;
 import org.apache.commons.lang.StringUtils;
-import org.apache.linkis.computation.client.LinkisJobBuilder;
+import org.apache.linkis.common.conf.Configuration;
+import org.apache.linkis.common.conf.Configuration$;
+import org.apache.linkis.common.exception.LinkisRetryException;
+import org.apache.linkis.common.utils.DefaultRetryHandler;
+import org.apache.linkis.common.utils.RetryHandler;
 import org.apache.linkis.computation.client.LinkisJobClient;
+import org.apache.linkis.computation.client.LinkisJobClient$;
+import org.apache.linkis.httpclient.config.ClientConfig;
+import org.apache.linkis.httpclient.dws.authentication.TokenAuthenticationStrategy;
+import org.apache.linkis.httpclient.dws.config.DWSClientConfig;
+import org.apache.linkis.httpclient.dws.config.DWSClientConfigBuilder;
+import org.apache.linkis.httpclient.dws.config.DWSClientConfigBuilder$;
 
 import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Linkis task launcher
@@ -36,8 +45,25 @@ public class LinkisExchangisTaskLauncher implements ExchangisTaskLauncher<Launch
     public void init(ExchangisTaskLaunchManager jobLaunchManager) {
         this.engineVersions.put(EngineTypeEnum.SQOOP.name().toLowerCase(), "1.4.6");
         this.engineVersions.put(EngineTypeEnum.DATAX.name().toLowerCase(), "3.0.0");
-        LinkisJobClient.config().setDefaultAuthToken(ClientConfiguration.LINKIS_TOKEN_VALUE.getValue());
-        LinkisJobClient.config().setDefaultServerUrl(ClientConfiguration.LINKIS_SERVER_URL.getValue());
+        RetryHandler retryHandler = new DefaultRetryHandler(){};
+        retryHandler.addRetryException(LinkisRetryException.class);
+        ClientConfig clientConfig = DWSClientConfigBuilder$.MODULE$
+                .newBuilder()
+                .setDWSVersion(Configuration.LINKIS_WEB_VERSION().getValue())
+                .addServerUrl(ClientConfiguration.LINKIS_SERVER_URL.getValue())
+                .connectionTimeout(45000)
+                .discoveryEnabled(false)
+                .discoveryFrequency(1, TimeUnit.MINUTES)
+                .loadbalancerEnabled(false)
+                .maxConnectionSize(ClientConfiguration.LINKIS_DEFAULT_MAX_CONNECTIONS.getValue())
+                .retryEnabled(true)
+                .setRetryHandler(retryHandler)
+                .readTimeout(90000) // We think 90s is enough, if SocketTimeoutException is throw, just set a new clientConfig to modify it.
+                .setAuthenticationStrategy(new TokenAuthenticationStrategy())
+                .setAuthTokenKey(TokenAuthenticationStrategy.TOKEN_KEY())
+                .setAuthTokenValue(ClientConfiguration.LINKIS_TOKEN_VALUE.getValue())
+                .build();
+        LinkisJobClient$.MODULE$.config().setDefaultClientConfig((DWSClientConfig) clientConfig);
     }
 
     @Override
