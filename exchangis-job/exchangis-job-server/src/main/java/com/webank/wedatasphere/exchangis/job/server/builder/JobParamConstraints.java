70cbef905f362eff19634f1f2e746fbb870f0292
jefftlin <jefft_lin@163.com>
2022-09-23 15:15:31 +0800

    Update jobParamDefine in oralce

--- exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/builder/JobParamConstraints.java
+++ exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/builder/JobParamConstraints.java
@@ -25,6 +25,8 @@ public class JobParamConstraints {
 
     public static final String PORT = "port";
 
+    public static final String SERVICE_NAME = "serviceName";
+
     public static final String WHERE = "where";
 
     public static final String WRITE_MODE = "writeMode";
