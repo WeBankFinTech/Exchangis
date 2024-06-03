dcc069ff2bcc575922eb5afb7b39d157365402d4
jefftlin <jefft_lin@163.com>
2024-06-03 22:13:33 +0800

    Minor fix

--- exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/service/impl/ExchangisJobDsBindServiceImpl.java
+++ exchangis-job/exchangis-job-server/src/main/java/com/webank/wedatasphere/exchangis/job/server/service/impl/ExchangisJobDsBindServiceImpl.java
@@ -32,7 +32,7 @@ public class ExchangisJobDsBindServiceImpl implements ExchangisJobDsBindService 
     public boolean inUse(Long datasourceId) {
         QueryWrapper<ExchangisJobDsBind> condition = new QueryWrapper<>();
         condition.eq("source_ds_id", datasourceId).or().eq("sink_ds_id", datasourceId);
-        int count = Optional.ofNullable(this.dsBindMapper.selectCount(condition)).orElse(0);
+        Integer count = Optional.ofNullable(this.dsBindMapper.selectCount(condition)).orElse(0);
         return count > 0;
     }
 }
